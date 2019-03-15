7e361fcd0636696199dc30cae5f57b03228e572b
Jan Faracik <j.faracik@kainos.com>
2019-03-15 12:00:38 +0000

    Added base html + removed sql

--- hub/views.py
+++ hub/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
-    template = loader.get_template('hub/index.html')
     context = {
         'latest_question_list': "banana",
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'hub/index.html', context)
