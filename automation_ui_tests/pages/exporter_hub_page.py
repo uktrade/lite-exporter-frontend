f980309165c60ec20fb9f67a1f64195b8eb87a24
Wisdom Ebohoin <>
2019-03-29 14:59:35 +0000

    e2e tests

---
+++ automation_ui_tests/pages/exporter_hub_page.py
@@ -0,0 +1,19 @@
+class ExporterHubPage():
+
+    # called e time you create an object for this class
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.apply_for_a_licence_btn = "a[href*='/new-application/']"
+        self.drafts_btn = "a[href*='/drafts/']"
+        self.applications_btn = "a[href*='/applications/']"
+
+    def click_apply_for_a_licence(self):
+        self.driver.find_element_by_css_selector(self.apply_for_a_licence_btn).click()
+
+    def click_drafts(self):
+        self.driver.find_element_by_css_selector(self.drafts_btn).click()
+
+    def click_applications(self):
+        self.driver.find_element_by_css_selector(self.applications_btn).click()
+
