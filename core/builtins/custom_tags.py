afc670c931113a3611f2298f498e6df67cc8c80f
Jan Faracik <43062514+jfaracik@users.noreply.github.com>
2019-04-04 11:54:18 +0100

    fix: JSON/SCSS Refactor
    * Simplified JSON loading, removed jsondate library
    
    * Fixed JSON dates in Drafts and Application
    
    * Now showing correct date on overview page
    
    * Now compress CSS
    
    * Increased width of full_width pages, fixed margin issue
    
    * Resolved issues, reduced max page width
    
    * Content Library Selector now throws a 500 error instead of displaying default text

--- core/builtins/custom_tags.py
+++ core/builtins/custom_tags.py
@@ -1,10 +1,20 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 from conf import strings
+import datetime
+
+from conf.constants import ISO8601_FMT
 
 register = template.Library()
 
 
 @register.simple_tag
 def get_string(value):
-    return strings.constants.get(value, 'COULDN\'T FIND STRING')
+    return strings.constants[value]
+
+
+@register.filter
+@stringfilter
+def str_date(value):
+    return datetime.datetime.strptime(value, ISO8601_FMT)
