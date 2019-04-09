7097c920fedbdcd4aa9ef50e32c9b10270cf86df
Jan Faracik <j.faracik@kainos.com>
2019-04-09 13:28:44 +0100

    Cleaned up views.py

--- new_application/views.py
+++ new_application/views.py
@@ -112,8 +112,7 @@ def form(request, pk):
 
 def overview(request):
     draft_id = request.GET.get('id')
-    response = requests.get(env("LITE_API_URL") + '/drafts/' + draft_id)
-    data = response.json()
+    data = requests.get(env("LITE_API_URL") + '/drafts/' + draft_id).json()
 
     context = {
         'title': 'Overview',
@@ -126,9 +125,8 @@ def overview(request):
 
 def submit(request):
     draft_id = request.GET.get('id')
-    response = requests.post(env("LITE_API_URL") + '/applications/',
-                             json={'id': draft_id})
-    data = json.loads(response.text)
+    data = requests.post(env("LITE_API_URL") + '/applications/',
+                         json={'id': draft_id}).json()
 
     if 'errors' in data:
         raise Http404
