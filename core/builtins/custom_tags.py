741467dd24474124709a49247e6d4b5493ea280d
Anirban Lahiri <a.lahiri@kainos.com>
2019-04-15 09:38:15 +0100

    minor additions to make the exporter-frontend compatible with LT-957

--- core/builtins/custom_tags.py
+++ core/builtins/custom_tags.py
@@ -3,6 +3,7 @@ from django.template.defaultfilters import stringfilter
 
 from conf import strings
 import datetime
+import stringcase
 
 from conf.constants import ISO8601_FMT
 
@@ -18,3 +19,8 @@ def get_string(value):
 @stringfilter
 def str_date(value):
     return datetime.datetime.strptime(value, ISO8601_FMT)
+
+
+@register.filter()
+def sentence_case(value):
+    return stringcase.sentencecase(value)
