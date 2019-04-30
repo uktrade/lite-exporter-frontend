479534d1cd281f14f07116fe9d59df8afb1caf95
Jan Faracik <j.faracik@kainos.com>
2019-04-30 17:01:57 +0100

    Added files for sites

--- sites/urls.py
+++ sites/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 app_name = 'sites'
 urlpatterns = [
-    # ex: /
+    # ex: /sites/
     path('', views.Sites.as_view(), name='sites'),
+    # ex: /sites/new/
+    path('new/', views.NewSite.as_view(), name='new'),
+    # ex: /sites/new/
+    path('<uuid:pk>/edit/', views.EditSite.as_view(), name='edit'),
 ]
--- sites/views.py
+++ sites/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from sites.services import get_sites
+from sites import forms
 
 
 class Sites(TemplateView):
@@ -12,21 +12,41 @@ class Sites(TemplateView):
             'title': 'Sites',
             'data': {'sites': [
                 {
-                    'id': '123',
+                    'id': '0fadea61-3ce1-4a31-abc3-4ad1ae677f3c',
                     'name': 'SITE1',
                 },
                 {
-                    'id': '123',
+                    'id': '0fadea61-3ce1-4a31-abc3-4ad1ae677f3c',
                     'name': 'SITE1',
                 },
                 {
-                    'id': '123',
+                    'id': '0fadea61-3ce1-4a31-abc3-4ad1ae677f3c',
                     'name': 'SITE1',
                 },
                 {
-                    'id': '123',
+                    'id': '0fadea61-3ce1-4a31-abc3-4ad1ae677f3c',
                     'name': 'SITE1',
                 }
             ]},
         }
         return render(request, 'sites/index.html', context)
+
+
+class NewSite(TemplateView):
+    def get(self, request, **kwargs):
+        context = {
+            'title': 'New Site',
+            'page': forms.new_site_form
+        }
+        return render(request, 'form/form.html', context)
+
+
+class EditSite(TemplateView):
+    def get(self, request, **kwargs):
+        # data, status_code = get_site(request, pk)
+
+        context = {
+            'title': 'Edit Site',
+            'page': forms.edit_site_form
+        }
+        return render(request, 'form/form.html', context)
