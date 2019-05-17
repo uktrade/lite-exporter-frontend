0b686408f637b09313cefe8474b4cf42d5323181
Jan Faracik <j.faracik@kainos.com>
2019-05-17 15:43:17 +0100

    Added initial page

--- new_application/forms.py
+++ new_application/forms.py
@@ -1,7 +1,30 @@
 from libraries.forms.components import Section, Form, Question, InputType, HTMLBlock, HiddenField, SideBySideSection, \
-    ArrayQuestion
+    ArrayQuestion, Option, DetailComponent, Button
 
 section1 = Section("Application Information", "", [
+    Form('Which export licence do you want to apply for?', 'Select one of the options.', [
+        ArrayQuestion(title='',
+                      description='',
+                      input_type=InputType.RADIOBUTTONS,
+                      name='licence_type',
+                      data=[
+                        Option(key='standard_licence', value='Standard Individual Export Licence (SIEL)',
+                               description='SIELs are specific to the company and the recipient (consignee). '
+                                           'They are for a set quantity and set value of goods. '
+                                           'You will need to provide support documentation with your application.'),
+                        Option(key='open_licence', value='Open Individual Export Licence (OIEL)',
+                               description='OIELs cover long-term projects and repeat business. '
+                                           'This is company specific, with no set quantity or value of goods. '
+                                           'You will receive compliance audits under this type of licence.'),
+                      ]),
+        DetailComponent('Help with choosing a licence', 'If you\'re unsure about which licence to select, '
+                                                        'then please read the guidance on GOV.UK about licences for '
+                                                        '<a class="govuk-link" '
+                                                        'href="https://www.gov.uk/starting-to-export/licences">'
+                                                        'exporting and doing business abroad</a>.',)
+    ], buttons=[
+        Button('Save and continue', 'submit')
+    ]),
     Form("Enter a name or reference for your application", "This can make it easier to find in the future.", [
         Question(title='',
                  description='',
@@ -33,9 +56,9 @@ def preexisting_good_form(id, description, control_code, part_number, units):
     return Form('Add a pre-existing good to your application', '', [
         HTMLBlock('<div class="govuk-inset-text">'
                   '<p><span style="opacity: 0.6;">Description:</span> ' + description + '</p>'
-                  '<p><span style="opacity: 0.6;">Control Code:</span> ' + control_code + '</p>'
-                  '<p><span style="opacity: 0.6;">Part Number:</span> ' + part_number + '</p>'
-                  '</div>'),
+                                                                                        '<p><span style="opacity: 0.6;">Control Code:</span> ' + control_code + '</p>'
+                                                                                                                                                                '<p><span style="opacity: 0.6;">Part Number:</span> ' + part_number + '</p>'
+                                                                                                                                                                                                                                      '</div>'),
         HiddenField(name='good_id',
                     value=id),
         Question(title='What\'s the value of your goods?',
