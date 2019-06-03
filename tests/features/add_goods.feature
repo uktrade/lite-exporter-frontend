231afbd2cefc9aca8f1561bf584ee06adc13d1ad
Nick Philpott <nicholasp@kainos.com>
2019-06-03 14:40:36 +0100

    goods test

--- tests/features/add_goods.feature
+++ tests/features/add_goods.feature
@@ -7,8 +7,8 @@ As a...
         When I login to exporter homepage with username "test@mail.com" and "password"
         When I click on goods link
         When I add a good with description "Good T1" controlled "Yes" control code "1234" incorporated "Yes" and part number "321"
+        Then I see good in goods list
         When I add a good with description "Good T2" controlled "Yes" control code "2345" incorporated "Yes" and part number "3456"
+        Then I see good in goods list
         When I add a good with description "Good T3" controlled "Yes" control code "535" incorporated "Yes" and part number "111"
-        Then I see good "Good T1" in goods list part number "321" control code "1234"
-        Then I see good "Good T2" in goods list part number "3456" control code "2345"
-        Then I see good "Good T3" in goods list part number "111" control code "535"
+        Then I see good in goods list
--- tests/step_defs/01_goods_setup_test.py
+++ tests/step_defs/01_goods_setup_test.py
@@ -1,10 +1,11 @@
+import random
 from pytest_bdd import scenarios, given, when, then, parsers
-from selenium.webdriver.common.by import By
-import helpers.helpers as utils
-from pages.exporter_hub_page import ExporterHubPage
 from pages.goods_list import GoodsList
+from pages.exporter_hub_page import ExporterHubPage
+from pages.add_goods_page import AddGoodPage
+from conftest import context
 
-scenarios('../features/add_goods.feature')
+scenarios('../features/add_goods.feature', strict_gherkin=False)
 
 
 @when('I click on goods link')
@@ -16,17 +17,21 @@ def click_my_goods_link(driver):
 @when(parsers.parse('I add a good with description "{description}" controlled "{controlled}" control code "{controlcode}" incorporated "{incorporated}" and part number "{part}"'))
 def add_new_good(driver, description, controlled,  controlcode, incorporated, part):
     exporter_hub = ExporterHubPage(driver)
-    exists = utils.is_element_present(driver, By.XPATH, "//*[text()[contains(.,'"+description+"')]]")
-    if not exists:
-        exporter_hub.click_add_a_good()
-        exporter_hub.enter_description_of_goods(description)
-        exporter_hub.select_is_your_good_controlled(controlled)
-        exporter_hub.enter_control_code(controlcode)
-        exporter_hub.select_is_your_good_intended_to_be_incorporated_into_an_end_product(incorporated)
-        exporter_hub.enter_part_number(part)
-        exporter_hub.click_save_and_continue()
+    add_goods_page = AddGoodPage(driver)
+    good_description = description + str(random.randint(1, 100))
+    context.good_description = good_description
+    context.part = part
+    context.controlcode = controlcode
+    add_goods_page.click_add_a_good()
+    add_goods_page.enter_description_of_goods(good_description)
+    add_goods_page.select_is_your_good_controlled(controlled)
+    add_goods_page.enter_control_code(controlcode)
+    add_goods_page.select_is_your_good_intended_to_be_incorporated_into_an_end_product(incorporated)
+    add_goods_page.enter_part_number(part)
+    exporter_hub.click_save_and_continue()
 
-@then(parsers.parse('I see good "{good}" in goods list part number "{partno}" control code "{controlcode}"'))
-def assert_good_is_in_list(driver, good, partno, controlcode):
+
+@then('I see good in goods list')
+def assert_good_is_in_list(driver):
     goods_list = GoodsList(driver)
-    goods_list.assert_goods_are_displayed_of_good_name(good, partno, controlcode)
+    goods_list.assert_goods_are_displayed_of_good_name(context.good_description, context.part, context.controlcode)
