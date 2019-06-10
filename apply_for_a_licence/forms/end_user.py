f97d99cac876642b68f216a0cb04fec88a01f08f
Jan Faracik <43062514+jfaracik@users.noreply.github.com>
2019-06-10 13:58:51 +0100

    fix: Added hardcoded PKs to forms as they no longer auto generate in the same way

--- apply_for_a_licence/forms/end_user.py
+++ apply_for_a_licence/forms/end_user.py
@@ -1,6 +1,7 @@
 from core.services import get_countries
 from libraries.forms.components import Form, Question, InputType, Section, ArrayQuestion, Option
 
+
 def new_end_user_form():
     return Section('', '', [
         Form(title='Who will be the final recipient (end-user) of your goods?',
@@ -17,18 +18,21 @@ def new_end_user_form():
                                    Option('other', 'Other', show_or=True),
                                ]),
              ],
+             pk='1',
              default_button_name='Continue'),
         Form(title='Enter the final recipient\'s name',
              description='',
              questions=[
                  Question('', '', InputType.INPUT, 'name'),
              ],
+             pk='2',
              default_button_name='Continue'),
         Form(title='Enter the final recipient\'s web address (URL)',
              description='',
              questions=[
                  Question('', '', InputType.INPUT, 'website', optional=True),
              ],
+             pk='3',
              default_button_name='Continue'),
         Form(title='Where\'s the final recipient based?',
              description='',
@@ -40,5 +44,6 @@ def new_end_user_form():
                                name='country',
                                data=get_countries(None, True)),
              ],
+             pk='4',
              default_button_name='Save and continue')
     ])
