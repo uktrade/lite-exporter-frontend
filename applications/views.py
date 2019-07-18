dd5c7b72726c28e75c39e8b55b62909017a0ffa1
Jan Faracik <j.faracik@kainos.com>
2019-07-18 10:50:46 +0100

    Added case notes and validation

--- applications/views.py
+++ applications/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 from applications.services import get_applications, get_application, post_application_notes
+from libraries.forms.generators import error_page
 
 
 class ApplicationsList(TemplateView):
@@ -43,7 +44,18 @@ class CaseNotes(TemplateView):
 
         response, status_code = post_application_notes(request, case_id, request.POST)
 
-        # if status_code is not 200:
-        #     return HttpResponse(status=status_code)
+        if status_code != 201:
+            errors = response.get('errors')
+            if errors.get('text'):
+                error = errors.get('text')[0]
+                error = error.replace('This field', 'Case note')
+                error = error.replace('this field', 'the case note')  # TODO: Move to API
+
+            else:
+                error_list = []
+                for key in errors:
+                    error_list.append("{field}: {error}".format(field=key, error=errors[key][0]))
+                error = "\n".join(error_list)
+            return error_page(request, error)
 
         return redirect(reverse_lazy('applications:application', kwargs={'pk': application_id}))
