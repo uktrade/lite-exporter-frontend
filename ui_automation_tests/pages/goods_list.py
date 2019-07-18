a8a777db2c94077554ab0c68394b6ec7a79de4c6
Nick Philpott <nicholasp@kainos.com>
2019-07-18 16:49:36 +0100

    Update goods_list.py

--- ui_automation_tests/pages/goods_list.py
+++ ui_automation_tests/pages/goods_list.py
@@ -10,17 +10,12 @@ class GoodsList():
         self.control_code_xpath_prefix = "%s/../*[text()[contains(.,'%s')]]"
 
     def assert_goods_are_displayed_of_good_name(self, description, part_number, control_code):
-        description_xpath = self.description_xpath_prefix % description
-        part_number_xpath = self.part_number_xpath_prefix % (description_xpath, part_number)
-        control_code_xpath = self.control_code_xpath_prefix % (description_xpath, control_code)
 
-        goods_row = self.driver.find_element_by_xpath(description_xpath)
-        goods_row_part_number = self.driver.find_element_by_xpath(part_number_xpath)
-        goods_row_control_code = self.driver.find_element_by_xpath(control_code_xpath)
+        goods_row = self.driver.find_element_by_css_selector(".govuk-table").text
 
-        assert goods_row.is_displayed()
-        assert goods_row_part_number.is_displayed()
-        assert goods_row_control_code.is_displayed()
+        assert description in goods_row
+        assert part_number in goods_row
+        assert control_code in goods_row
 
     def click_on_goods_edit_link(self, number):
         self.driver.find_elements_by_css_selector(self.goods_edit_link)[number].click()
