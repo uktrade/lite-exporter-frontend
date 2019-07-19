e4e841319cecb33c3c4e0bd203b793c5d8161c58
Jan Faracik <j.faracik@kainos.com>
2019-07-19 13:47:36 +0100

    cleanup

--- ui_automation_tests/step_defs/goods_test.py
+++ ui_automation_tests/step_defs/goods_test.py
@@ -4,7 +4,7 @@ from pages.exporter_hub_page import ExporterHubPage
 from pages.add_goods_page import AddGoodPage
 from conftest import context
 
-from ui_automation_tests.helpers.helpers import get_element_index_by_text
+from helpers.helpers import get_element_index_by_text
 
 scenarios('../features/add_goods.feature', strict_gherkin=False)
 
@@ -12,11 +12,11 @@ scenarios('../features/add_goods.feature', strict_gherkin=False)
 @then('I see good in goods list')
 def assert_good_is_in_list(driver):
     goods_list = GoodsList(driver)
-    goods_list.assert_goods_are_displayed_of_good_name(context.good_description, context.part, context.controlcode)
+    goods_list.assert_goods_are_displayed_of_good_name(context.good_description, context.part, context.control_code)
 
 
-@when(parsers.parse('I edit a good to description "{description}" controlled "{controlled}" control code "{controlcode}" incorporated "{incorporated}" and part number "{part}"'))
-def edit_good(driver, description, controlled,  controlcode, incorporated, part):
+@when(parsers.parse('I edit a good to description "{description}" controlled "{controlled}" control code "{control_code}" incorporated "{incorporated}" and part number "{part}"'))
+def edit_good(driver, description, controlled,  control_code, incorporated, part):
     exporter_hub = ExporterHubPage(driver)
     add_goods_page = AddGoodPage(driver)
     goods_list = GoodsList(driver)
@@ -44,4 +44,3 @@ def delete_my_good_in_list(driver):
 @then('my good is no longer in the goods list')
 def good_is_no_longer_in_list(driver):
     assert context.edited_description not in driver.find_element_by_tag_name('body').text
-
