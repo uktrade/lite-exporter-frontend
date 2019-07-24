822c70113819569003d6a242f56a1e6b13dfaa63
cstobart <c.stobart@kainos.com>
2019-07-24 10:39:45 +0100

    remove username/password login

--- core/views.py
+++ core/views.py
@@ -1,14 +1,8 @@
-import requests
-from django.contrib.auth import login as django_login, logout as django_logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.utils.decorators import decorator_from_middleware
 
-from conf.middleware import ProtectAllViewsMiddleware
-from conf.settings import env
 from core.builtins.custom_tags import get_string
-from core.helpers import Section, Tile, generate_notification_string
-from core.models import User
+from core.helpers import Section, Tile
 from core.services import get_notifications, get_clc_notifications
 
 
@@ -39,51 +33,3 @@ def hub(request):
         'applicationDeleted': request.GET.get('application_deleted'),
     }
     return render(request, 'core/hub.html', context)
-
-
-def login(request):
-    if request.method == 'GET':
-        context = {
-            'title': get_string('misc.sign_in'),
-        }
-        return render(request, 'core/login.html', context)
-
-    if request.method == 'POST':
-        response = requests.post(env('LITE_API_URL') + '/users/authenticate/',
-                                 json={
-                                     'email': request.POST.get('email'),
-                                     'password': request.POST.get('password'),
-                                 },
-                                 )
-
-        # If login isn't successful, return previous page
-        if response.status_code is not 200:
-            context = {
-                'title': get_string('misc.sign_in'),
-                'error': True,
-                'email': request.POST.get('email'),
-            }
-            return render(request, 'core/login.html', context)
-
-        user_data = response.json().get('user')
-
-        user_object, created = User.objects.get_or_create(
-            id=user_data.get('id'), defaults={
-                'email': user_data.get('email'),
-                'first_name': user_data.get('first_name'),
-                'last_name': user_data.get('last_name'),
-        })
-
-        django_login(request, user=user_object)
-
-        # Redirect to index page as a signed in user
-        return redirect('/')
-
-
-def logout(request):
-    django_logout(request)
-
-    context = {
-        'title': get_string('misc.signed_out'),
-    }
-    return render(request, 'core/loggedout.html', context)
