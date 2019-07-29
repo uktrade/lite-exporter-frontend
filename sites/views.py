4fb13504b948eb98a47ecfb29cdc972f9d98f6ad
Jan Faracik <j.faracik@kainos.com>
2019-07-29 10:18:49 +0100

    Cleanup

--- sites/views.py
+++ sites/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
+from libraries.forms.generators import form_page
 from libraries.forms.helpers import nest_data, flatten_data
 from sites import forms
+from sites.forms import new_site_form, edit_site_form
 from sites.services import get_sites, get_site, post_sites, put_site
 
 
@@ -18,12 +21,10 @@ class Sites(TemplateView):
 
 
 class NewSite(TemplateView):
+    form = new_site_form()
+
     def get(self, request, **kwargs):
-        context = {
-            'title': 'New Site',
-            'page': forms.new_site_form(),
-        }
-        return render(request, 'form.html', context)
+        return form_page(request, self.form)
 
     def post(self, request, **kwargs):
         data = request.POST.copy()
@@ -33,27 +34,18 @@ class NewSite(TemplateView):
         validated_data, status_code = post_sites(request, nested_data)
 
         if 'errors' in validated_data:
-            context = {
-                'page': forms.new_site_form(),
-                'title': forms.new_site_form().title,
-                'errors': validated_data['errors'],
-                'data': data,
-            }
-            return render(request, 'form.html', context)
+            validated_data['errors'] = flatten_data(validated_data['errors'])
+            return form_page(request, self.form, data=request.POST, errors=validated_data['errors'])
 
-        return redirect('/sites/')
+        return redirect(reverse_lazy('sites:sites'))
 
 
 class EditSite(TemplateView):
+    form = edit_site_form()
+
     def get(self, request, **kwargs):
         site, status_code = get_site(request, str(kwargs['pk']))
-
-        context = {
-            'title': 'Edit Site',
-            'page': forms.edit_site_form(),
-            'data': flatten_data(site.get('site')),
-        }
-        return render(request, 'form.html', context)
+        return form_page(request, self.form, data=flatten_data(site.get('site')))
 
     def post(self, request, **kwargs):
         validated_data, status_code = put_site(request, str(kwargs['pk']), json=nest_data(request.POST))
@@ -67,4 +59,4 @@ class EditSite(TemplateView):
             }
             return render(request, 'form.html', context)
 
-        return redirect('/sites/')
+        return redirect(reverse_lazy('sites:sites'))
