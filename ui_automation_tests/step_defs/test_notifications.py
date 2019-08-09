070e8b086a7671ad1bf2f59523c16cae1881f2bf
annevjenkins-lite <47815147+annevjenkins-lite@users.noreply.github.com>
2019-08-09 15:49:50 +0100

    Added test step to test notification appearing in application list

--- ui_automation_tests/step_defs/test_notifications.py
+++ ui_automation_tests/step_defs/test_notifications.py
@@ -10,7 +10,7 @@ def application_exists_case_note_added(add_an_application, internal_case_note):
     pass
 
 
-@then('I can see a notification')
+@then('I can see a notification in application tile')
 def notification_exists(driver, context):
     exporter_hub_page = ExporterHubPage(driver)
     context.number_of_notifications = exporter_hub_page.return_number_of_notifications()
@@ -19,11 +19,16 @@ def notification_exists(driver, context):
 
 @when('I click on my application')
 def click_on_application(driver, context):
-    exporter_hub_page = ExporterHubPage(driver)
-    exporter_hub_page.click_applications()
     elements = driver.find_elements_by_css_selector(".govuk-table__cell a")
     app_num = helpers.get_element_index_by_text(elements, context.app_id)
     elements[app_num].click()
+
+
+@then('I see a notification on application list')
+def notification_on_application_list(driver, context):
+    elements = driver.find_elements_by_css_selector(".govuk-table__row")
+    app_num = helpers.get_element_index_by_partial_text(elements, context.app_id)
+    assert "New notification" in elements[app_num].text
 
 
 @then('I can see the internally added note')
