daac6a33f25dba56936fa74986b2a1a7fac89346
annevjenkins-lite <47815147+annevjenkins-lite@users.noreply.github.com>
2019-08-12 09:56:11 +0100

    Renamed app_num variable to app_id following review comments

--- ui_automation_tests/step_defs/test_notifications.py
+++ ui_automation_tests/step_defs/test_notifications.py
@@ -20,15 +20,15 @@ def notification_exists(driver, context):
 @when('I click on my application')
 def click_on_application(driver, context):
     elements = driver.find_elements_by_css_selector(".govuk-table__cell a")
-    app_num = helpers.get_element_index_by_text(elements, context.app_id)
-    elements[app_num].click()
+    app_id = helpers.get_element_index_by_text(elements, context.app_id)
+    elements[app_id].click()
 
 
 @then('I see a notification on application list')
 def notification_on_application_list(driver, context):
     elements = driver.find_elements_by_css_selector(".govuk-table__row")
-    app_num = helpers.get_element_index_by_partial_text(elements, context.app_id)
-    assert "New notification" in elements[app_num].text
+    app_id = helpers.get_element_index_by_partial_text(elements, context.app_id)
+    assert "New notification" in elements[app_id].text
 
 
 @then('I can see the internally added note')
