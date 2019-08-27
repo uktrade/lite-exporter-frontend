8a94e3dbf1586b64849748d6e5cbb19b86062b0e
Nick Philpott <nicholasp@kainos.com>
2019-08-27 14:01:44 +0100

    tests

--- ui_automation_tests/pages/shared.py
+++ ui_automation_tests/pages/shared.py
@@ -4,6 +4,8 @@ class Shared:
         self.driver = driver
         self.submit_button = "button[type*='submit']"
         self.error_message = ".govuk-error-message"
+        self.gov_body = ".govuk-error-message"
+        self.gov_table_cell_links = "..govuk-table__cell a"
 
     def get_text_of_error_message(self, position=0):
         return self.driver.find_elements_by_css_selector(self.error_message)[position].text
@@ -16,4 +18,10 @@ class Shared:
 
     def get_text_of_body(self):
         return self.driver.find_element_by_tag_name("body").text
+
+    def get_text_of_gov_body(self):
+        return self.driver.find_element_by_css_selector(self.gov_body).text
+
+    def get_gov_table_cell_links(self):
+        return self.driver.find_elements_by_css_selector(self.gov_table_cell_links)
 
