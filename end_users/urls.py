e51f6dbca8fcbcbadd6747fc9b7a28f12acb8d5b
Jan Faracik <j.faracik@kainos.com>
2019-09-18 15:35:43 +0100

    Update urls.py

--- end_users/urls.py
+++ end_users/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     # ex: /end-users/
     path('', views.EndUsersList.as_view(), name='end_users'),
 
-    # ex: /<int:pk>/copy/
-    path('<int:pk>/copy/', views.CopyAdvisory.as_view(), name='copy'),
+    # ex: /<int:pk>/copy-advisory/
+    path('<int:pk>/copy-advisory/', views.CopyAdvisory.as_view(), name='copy'),
 
     # ex: /end-users/apply-for-an-advisory/
     path('apply-for-an-advisory/', views.ApplyForAnAdvisory.as_view(), name='apply'),
