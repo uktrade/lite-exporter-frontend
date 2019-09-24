ee1acfba35b0c1514c6414844c261497342f9799
Mark Spiers <marksp@kainos.com>
2019-09-24 12:35:59 +0100

    end_user page implemented

--- end_users/urls.py
+++ end_users/urls.py
@@ -7,10 +7,12 @@ app_name = 'end_users'
 urlpatterns = [
     # ex: /end-users/
     path('', views.EndUsersList.as_view(), name='end_users'),
-
-    # ex: /<int:pk>/copy-advisory/
+    # ex: /end-users/<int:pk>/
+    path('<int:pk>/', views.EndUserDetailEmpty.as_view(), name='end_user'),
+    # ex: /end-users/<int:pk>/copy-advisory/
     path('<int:pk>/copy-advisory/', views.CopyAdvisory.as_view(), name='copy'),
-
     # ex: /end-users/apply-for-an-advisory/
     path('apply-for-an-advisory/', views.ApplyForAnAdvisory.as_view(), name='apply'),
+    # ex: /end-users/<int:pk>/<str:type>/
+    path('<int:pk>/<str:type>/', views.EndUserDetail.as_view(), name='end_user_detail'),
 ]
--- end_users/views.py
+++ end_users/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from lite_forms.components import HiddenField
-from lite_forms.generators import form_page
+from lite_forms.generators import form_page, error_page
 from lite_forms.submitters import submit_paged_form
 
+from applications.services import get_case_notes, get_application_ecju_queries, post_application_case_notes
+from core.services import get_notifications
 from end_users.forms import apply_for_an_end_user_advisory_form, copy_end_user_advisory_form, \
     end_user_advisory_success_page
 from end_users.services import get_end_user_advisories, post_end_user_advisories, get_end_user_advisory
@@ -17,7 +21,7 @@ class EndUsersList(TemplateView):
             'title': 'End User Advisories',
             'end_users': end_users,
         }
-        return render(request, 'end_users/index.html', context)
+        return render(request, 'end_users/end_users.html', context)
 
 
 class CopyAdvisory(TemplateView):
@@ -27,7 +31,7 @@ class CopyAdvisory(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         self.forms = copy_end_user_advisory_form()
-        query = get_end_user_advisory(request, str(kwargs['pk']))
+        query, _ = get_end_user_advisory(request, str(kwargs['pk']))
 
         # Add the existing end user type as a hidden field to preserve its data
         self.forms.forms[0].questions.append(HiddenField('end_user.sub_type', query['end_user']['sub_type']['key']))
@@ -75,3 +79,76 @@ class ApplyForAnAdvisory(TemplateView):
             return response
 
         return end_user_advisory_success_page(request, str(data['end_user_advisory']['id']))
+
+class EndUserDetailEmpty(TemplateView):
+    def get(self, request, **kwargs):
+        return redirect(reverse_lazy('end_users:end_user_detail', kwargs={'pk': kwargs['pk'],
+                                                                  'type': 'case-notes'}))
+
+
+class EndUserDetail(TemplateView):
+    end_user_advisory_id = None
+    end_user_advisory = None
+    view_type = None
+    case_id = None
+
+    def dispatch(self, request, *args, **kwargs):
+        self.end_user_advisory_id = str(kwargs['pk'])
+        self.end_user_advisory, self.case_id = get_end_user_advisory(request, self.end_user_advisory_id)
+        self.view_type = kwargs['type']
+
+        if self.view_type != 'case-notes' and self.view_type != 'ecju-queries':
+            return Http404
+
+        return super(EndUserDetail, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, **kwargs):
+        notifications = get_notifications(request, unviewed=True)
+        case_note_notifications = len([x for x in notifications if x['parent'] == self.end_user_advisory_id
+                                       and x['object'] == 'case_note'])
+        ecju_query_notifications = len([x for x in notifications if x['parent'] == self.end_user_advisory_id
+                                        and x['object_type'] == 'ecju_query'])
+
+        context = {
+            'title': 'Good',
+            'case_id': self.case_id,
+            'end_user_advisory': self.end_user_advisory,
+            'case_note_notifications': case_note_notifications,
+            'ecju_query_notifications': ecju_query_notifications,
+            'type': self.view_type
+        }
+
+        if self.view_type == 'case-notes':
+            case_notes = get_case_notes(request, self.case_id)['case_notes']
+            context['notes'] = filter(lambda note: note['is_visible_to_exporter'], case_notes)
+
+        if self.view_type == 'ecju-queries':
+            context['open_queries'], context['closed_queries'] = get_application_ecju_queries(request,
+                                                                                              self.case_id)
+
+        return render(request, 'end_users/end_user.html', context)
+
+    def post(self, request, **kwargs):
+        if self.view_type != 'case-notes':
+            return Http404
+
+        good_id = kwargs['pk']
+
+        response, _ = post_application_case_notes(request, self.case_id, request.POST)
+
+        if 'errors' in response:
+            errors = response.get('errors')
+            if errors.get('text'):
+                error = errors.get('text')[0]
+                error = error.replace('This field', 'Case note')
+                error = error.replace('this field', 'the case note')  # TODO: Move to API
+
+            else:
+                error_list = []
+                for key in errors:
+                    error_list.append("{field}: {error}".format(field=key, error=errors[key][0]))
+                error = "\n".join(error_list)
+            return error_page(request, error)
+
+        return redirect(reverse_lazy('end_users:end_user_detail', kwargs={'pk': self.end_user_advisory_id,
+                                                                  'type': 'case-notes'}))
