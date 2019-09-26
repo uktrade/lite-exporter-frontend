f5c62c151aadd713fe7caacea978ed20974dda4a
Jan Faracik <j.faracik@kainos.com>
2019-09-26 07:19:41 +0100

    Update utils.py

--- ui_automation_tests/helpers/utils.py
+++ ui_automation_tests/helpers/utils.py
@@ -12,10 +12,16 @@ class Timer:
         return time.time() - self.start
 
     def print_time(self, context):
-        print("timer: "+context+": " + str(self.get_time()))
+        print(f'Timer: {context}: {str(self.get_time())}')
 
 
-def get_or_create_attr(obj, attr, fn):
+def get_or_create_attr(obj, attr: str, fn):
+    """
+    Sets the named attribute on the given object to the specified value if it
+    doesn't exist, else it'll return the attribute
+
+    setattr(obj, 'attr', fn) is equivalent to ``obj['attr'] = fn''
+    """
     if not hasattr(obj, attr):
         setattr(obj, attr, fn())
     return getattr(obj, attr)
