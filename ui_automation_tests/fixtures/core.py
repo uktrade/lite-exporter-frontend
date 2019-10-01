5b3451fb31195175900e2abe5334ca72f367cf38
BenAndrew-Kainos <bena@kainos.com>
2019-10-01 15:11:46 +0100

    update fixtures

--- ui_automation_tests/fixtures/core.py
+++ ui_automation_tests/fixtures/core.py
@@ -50,27 +50,43 @@ def context(request):
     return Context()
 
 
-@fixture(scope="session")
-def exporter_sso_login_info(request):
+@fixture(scope='session')
+def exporter_info(request):
     exporter_sso_email = env('TEST_EXPORTER_SSO_EMAIL')
-    exporter_sso_password = env('TEST_EXPORTER_SSO_PASSWORD')
+    first_name = 'Test'
+    last_name = 'Lite'
 
-    return {'email': exporter_sso_email, 'password': exporter_sso_password}
+    return {
+        'email': exporter_sso_email,
+        'first_name': first_name,
+        'last_name': last_name
+    }
 
 
-@fixture(scope="session")
+@fixture(scope='session')
+def internal_info(request):
+    gov_user_email = env('TEST_SSO_EMAIL')
+    gov_user_first_name, gov_user_last_name = env('TEST_SSO_NAME').split(' ')
+
+    return {
+        'email': gov_user_email,
+        'first_name': gov_user_first_name,
+        'last_name': gov_user_last_name
+    }
+
+
+@fixture(scope='session')
 def s3_key(request):
     s3_key = env('TEST_S3_KEY')
     return s3_key
 
 
-@fixture(scope="session")
-def invalid_username(request):
-    return "invalid@mail.com"
-
-
-@fixture(scope="session")
-def seed_data_config(request, exporter_sso_login_info, s3_key):
-    exporter_sso_email = exporter_sso_login_info['email']
-    api_url = request.config.getoption("--lite_api_url")
-    return {'api_url': api_url, 'email': exporter_sso_email, 's3_key': s3_key}
+@fixture(scope='session')
+def seed_data_config(request, exporter_info, internal_info, s3_key):
+    api_url = request.config.getoption('--lite_api_url')
+    return {
+        'api_url': api_url,
+        'exporter': exporter_info,
+        'gov': internal_info,
+        's3_key': s3_key
+    }
