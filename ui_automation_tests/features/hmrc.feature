f6fc36acf964bbaffae5bd53274dd3f2679e30db
cstobart <c.stobart@kainos.com>
2019-10-03 12:17:26 +0100

    shared steps

--- ui_automation_tests/features/hmrc.feature
+++ ui_automation_tests/features/hmrc.feature
@@ -4,5 +4,7 @@ Feature: I want to be able to perform actions as a HMRC user
   @LT_1008_log_in_as_hmrc_and_select_organisation_to_raise_query_for
     Scenario: Select organisation to raise query for
       # This has the second org in the seed data as a hmrc org, this is to save on multiple accesses to great sso
-      Given I log into to a HMRC organisation
-      When I select to raise a query for my first organisation
+      Given I have a second set up organisation
+      And I go to exporter homepage and choose Test Org
+      When I switch organisations to my second organisation
+      And I select to raise a query for my first organisation
--- ui_automation_tests/step_defs/test_hmrc.py
+++ ui_automation_tests/step_defs/test_hmrc.py
@@ -1,20 +1,10 @@
-from pytest_bdd import when, then, scenarios, given
+from pytest_bdd import when, scenarios
 
-from helpers import helpers
 from pages.exporter_hub_page import ExporterHubPage
 from pages.hmrc_query_page import HMRCQueryPage
-from pages.hub_page import Hub
 from pages.shared import Shared
 
 scenarios('../features/hmrc.feature', strict_gherkin=False)
-
-
-@given("I log into to a HMRC organisation")
-def set_up_second_organisation(register_organisation_for_switching_organisation, driver, context):
-    Hub(driver).click_switch_link()
-    no = helpers.get_element_index_by_text(Shared(driver).get_radio_buttons_elements(), context.org_name_for_switching_organisations)
-    Shared(driver).click_on_radio_buttons(no)
-    Shared(driver).click_continue()
 
 
 @when("I select to raise a query for my first organisation")
