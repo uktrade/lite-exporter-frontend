3d7192f9a516fbc216a4b487d72f2a7810c128af
Jan Faracik <j.faracik@kainos.com>
2019-10-04 11:37:23 +0100

    Cleanup

--- conf/context_processors.py
+++ conf/context_processors.py
@@ -1,10 +1,11 @@
-import os
+from conf.settings import env
 
 
 def export_vars(request):
     data = {
-        'ENVIRONMENT_VARIABLES': dict(os.environ.items()),
         'SERVICE_NAME': 'LITE',
-        'GOV_UK_ADDRESS': 'https://www.gov.uk'
+        'GOV_UK_URL': 'https://www.gov.uk',
+        'FEEDBACK_URL': env('FEEDBACK_URL'),
+        'GOOGLE_ANALYTICS_KEY': env('GOOGLE_ANALYTICS_KEY'),
     }
     return data
