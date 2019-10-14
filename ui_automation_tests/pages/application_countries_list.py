1a61f70e0a88e662de08aa7d6074172630134125
BenAndrew-Kainos <bena@kainos.com>
2019-10-14 10:57:35 +0100

    fix open applications e2e test

--- ui_automation_tests/pages/application_countries_list.py
+++ ui_automation_tests/pages/application_countries_list.py
@@ -6,7 +6,7 @@ class ApplicationCountriesList:
         self.countries_labels = ".govuk-checkboxes__label"
         self.countries_search_box = "filter-box"   # ID
         self.countries_list = "pane_countries"   # ID
-        self.title = ".govuk-heading-l"
+        self.title = ".govuk-fieldset__heading"
 
     def get_countries_names(self):
         countries_names = []
