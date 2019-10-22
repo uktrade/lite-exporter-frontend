2f985143908a115ff4434902f2ed7f1d3de9242f
Jan Faracik <j.faracik@kainos.com>
2019-10-22 13:00:39 +0100

    Cleanup

--- assets/javascripts/specific/overview.js
+++
@@ -1,9 +0,0 @@
-function showCancelApplication() {
-	LITECommon.Modal.showModal("Are you sure you want to delete this application?", $("#cancel-application").html(), true);
-	return false;
-};
-
-function showCountries() {
-	LITECommon.Modal.showModal("Countries", $("#countries-data").html(), false);
-	return false;
-};
