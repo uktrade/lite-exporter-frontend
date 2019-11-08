0cc11da15fe499ed9969c3c7c508c05a058f6c80
Jan Faracik <j.faracik@kainos.com>
2019-11-08 16:02:03 +0000

    Update hmrc_query_page.py

--- ui_automation_tests/pages/hmrc_query_page.py
+++ ui_automation_tests/pages/hmrc_query_page.py
@@ -6,15 +6,15 @@ class HMRCQueryPage:
     def __init__(self, driver):
         self.driver = driver
 
-        self.govuk_input = '.govuk-input'
+        self.textbox = '.govuk-input'
         self.org_radio_gutton_id_part = 'organisation-'
         self.continue_button = "button[value='continue']"
 
     def search_for_org(self, name):
-        input = self.driver.find_element_by_css_selector(self.govuk_input)
-        input.clear()
-        input.send_keys(name)
-        input.send_keys(Keys.ENTER)
+        textbox = self.driver.find_element_by_css_selector(self.textbox)
+        textbox.clear()
+        textbox.send_keys(name)
+        textbox.send_keys(Keys.ENTER)
 
     def click_org_radio_button(self, org_id):
         self.driver.find_element_by_id(self.org_radio_gutton_id_part + org_id).click()
