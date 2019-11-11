975278189dae8941a401eb218b85aacc9332ab10
Jan Faracik <j.faracik@kainos.com>
2019-11-11 17:42:21 +0000

    Fix form

--- apply_for_a_licence/forms.py
+++ apply_for_a_licence/forms.py
@@ -5,41 +5,46 @@ from lite_forms.components import RadioButtons, Form, DetailComponent, TextInput
 
 def initial_questions():
     return FormGroup([
-        Form(get_string('applications.initial_questions.export_title'), [
-            RadioButtons(name='application_type',
-                         options=[
-                             Option(key=STANDARD_LICENCE, value='Standard Licence',
-                                    description='Standard Licences are specific to the company and the recipient (consignee). '
-                                                'They are for a set quantity and set value of goods. '
-                                                'You will need to provide support documentation with your application.'),
-                             Option(key=OPEN_LICENCE, value='Open Licence',
-                                    description='Open Licences cover long-term projects and repeat business. '
-                                                'This is company specific, with no set quantity or value of goods. '
-                                                'You will receive compliance audits under this type of licence.'),
-                         ]),
-            DetailComponent('Help with choosing a licence', 'If you\'re unsure about which licence to select, '
-                                                            'then read the guidance on GOV.UK for '
-                                                            '<a class="govuk-link" target="_blank"'
-                                                            'href="https://www.gov.uk/starting-to-export/licences">'
-                                                            'exporting and doing business abroad<span '
-                                                            'class="govuk-visually-hidden"> (Opens in a new window or '
-                                                            'tab)</span></a>.', )
-        ], default_button_name='Continue'),
-        Form(get_string('applications.initial_questions.reference_title'),
-             [
+        Form(title=get_string('applications.initial_questions.export_title'),
+             questions=[
+                 RadioButtons(name='application_type',
+                              options=[
+                                  Option(key=STANDARD_LICENCE, value='Standard Licence',
+                                         description='Standard Licences are specific to the company and the recipient (consignee). '
+                                                     'They are for a set quantity and set value of goods. '
+                                                     'You will need to provide support documentation with your application.'),
+                                  Option(key=OPEN_LICENCE, value='Open Licence',
+                                         description='Open Licences cover long-term projects and repeat business. '
+                                                     'This is company specific, with no set quantity or value of goods. '
+                                                     'You will receive compliance audits under this type of licence.'),
+                              ]),
+                 DetailComponent('Help with choosing a licence', 'If you\'re unsure about which licence to select, '
+                                                                 'then read the guidance on GOV.UK for '
+                                                                 '<a class="govuk-link" target="_blank"'
+                                                                 'href="https://www.gov.uk/starting-to-export/licences">'
+                                                                 'exporting and doing business abroad<span '
+                                                                 'class="govuk-visually-hidden"> (Opens in a new window or '
+                                                                 'tab)</span></a>.', )
+             ],
+             default_button_name='Continue'),
+        Form(title=get_string('applications.initial_questions.reference_title'),
+             questions=[
                  TextInput(name='name'),
-             ], default_button_name='Continue'),
-        Form('Do you want to export temporarily or permanently', '',
-             [
+             ],
+             default_button_name='Continue'),
+        Form(title='Do you want to export temporarily or permanently',
+             description='',
+             questions=[
                  RadioButtons(name='export_type',
                               options=[
                                   Option('temporary', 'Temporarily'),
                                   Option('permanent', 'Permanently')
                               ]),
-             ], default_button_name='Continue'),
-        Form('Have you been told that you need an export licence by an official?',
-             'This could be a letter or email from HMRC or another government department.',
-             [
+             ],
+             default_button_name='Continue'),
+        Form(title='Have you been told that you need an export licence by an official?',
+             description='This could be a letter or email from HMRC or another government department.',
+             questions=[
                  RadioButtons(name='have_you_been_informed',
                               options=[
                                   Option('yes', 'Yes',
