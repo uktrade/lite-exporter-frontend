86fc66273ee1ff52a3f6d9a668ed65e074853238
Jan Faracik <j.faracik@kainos.com>
2019-11-13 12:20:09 +0000

    Update with strings

--- applications/libraries/summaries.py
+++ applications/libraries/summaries.py
@@ -1,4 +1,5 @@
 from core.builtins.custom_tags import str_date
+from lite_content.lite_exporter_frontend import strings
 from lite_forms.components import Summary
 
 
@@ -8,9 +9,9 @@ def application_summary(application):
 
     return Summary(
         values={
-            'Reference': application['name'],
-            'Licence type': application['licence_type']['value'],
-            'Export type': application['export_type']['value'],
-            'Submitted at': str_date(application['submitted_at'])
+            strings.APPLICATION_REFERENCE_NAME: application['name'],
+            strings.APPLICATION_TYPE: application['licence_type']['value'],
+            strings.APPLICATION_EXPORT_TYPE: application['export_type']['value'],
+            strings.APPLICATION_SUBMITTED_AT: str_date(application['submitted_at'])
         },
         classes=['govuk-summary-list--no-border'])
