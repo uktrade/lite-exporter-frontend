0f66c40abc78a9f747c5a2c9c540e67f7fab85f0
Jan Faracik <j.faracik@kainos.com>
2019-11-15 17:30:08 +0000

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -202,6 +202,10 @@ MAX_UPLOAD_SIZE = 100*1024*1024
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+print('\n')
+print(env('ENVIRONMENT'))
+print('\n')
+
 if 'test' in sys.argv or env('ENVIRONMENT') == 'test':
     DATABASES = {
         'default': {
