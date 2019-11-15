2b3871acaef622ded358d87725e6505405e47f94
Jan Faracik <j.faracik@kainos.com>
2019-11-15 15:29:13 +0000

    Cleanup

--- applications/forms/countries.py
+++ applications/forms/countries.py
@@ -1,18 +1,20 @@
-from django.urls import reverse
-from lite_forms.components import Form, Filter, Checkboxes, BackLink
-
-from core.services import get_countries
+from applications.components import back_to_task_list
 from core.builtins.custom_tags import get_string
+from core.services import get_countries
+from lite_content.lite_exporter_frontend import strings
+from lite_forms.components import Form, Filter, Checkboxes
 
 
-def countries_form(draft_id):
+def countries_form(application_id):
     return Form(title=get_string('licences.countries.title'),
                 description=get_string('licences.countries.description'),
                 questions=[
                     Filter(),
-                    Checkboxes('countries', get_countries(None, True), classes=['govuk-checkboxes--small']),
+                    Checkboxes(name='countries',
+                               options=get_countries(None, True),
+                               classes=['govuk-checkboxes--small'],
+                               show_select_links=True),
                 ],
                 javascript_imports=['/assets/javascripts/filter-checkbox-list.js'],
-                default_button_name='Save',
-                back_link=BackLink(get_string('common.back_to_task_list'), reverse('applications:task_list',
-                                                                                   kwargs={'pk': draft_id})))
+                default_button_name=strings.SAVE,
+                back_link=back_to_task_list(application_id))
