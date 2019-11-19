e20eec710cf67e9d4f3c92594390662f168def4e
Nick Philpott <nicholasp@kainos.com>
2019-11-19 12:16:59 +0000

    update

--- ui_automation_tests/docker-entry.sh
+++ ui_automation_tests/docker-entry.sh
@@ -8,7 +8,7 @@ if [ $NO_OF_PARALLEL_RUNNERS -gt 1 ]
 then
     $parallel_option="-n=$NO_OF_PARALLEL_RUNNERS"
 fi
-py.test -k "$TESTS_TO_RUN" $parallel_option --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
+py.test -k "$TESTS_TO_RUN" "$parallel_option" --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
 pid="$!"
 
 # trap process id to stop script using Control+C
