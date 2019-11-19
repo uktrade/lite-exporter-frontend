a5bce0862fe44da7f8297399ebe49f91581750e8
Jan Faracik <j.faracik@kainos.com>
2019-11-19 14:23:43 +0000

    Update apply_for_a_licence_page.py

--- ui_automation_tests/pages/apply_for_a_licence_page.py
+++ ui_automation_tests/pages/apply_for_a_licence_page.py
@@ -1,27 +1,21 @@
-import logging
-
 from shared import functions
 from shared.BasePage import BasePage
-
-log = logging.getLogger()
-console = logging.StreamHandler()
-log.addHandler(console)
 
 
 class ApplyForALicencePage(BasePage):
 
-    name_or_reference_input_id = "name"
+    NAME_OR_REFERENCE_INPUT_ID = "name"
     BUTTON_STANDARD_LICENCE = "input#application_type-standard_licence"
     BUTTON_OPEN_LICENCE = "input#application_type-open_licence"
-    export_button = "export_type-"
-    export_licence_yes_or_no = "have_you_been_informed-"
-    reference_number = "reference_number_on_information_form"
-    success_message = ".govuk-panel__title"
-    application_is_submitted = ".govuk-panel__title"
+    EXPORT_BUTTON = "export_type-"
+    EXPORT_LICENCE_YES_OR_NO = "have_you_been_informed-"
+    REFERENCE_NUMBER = "reference_number_on_information_form"
+    SUCCESS_MESSAGE = ".govuk-panel__title"
+    APPLICATION_IS_SUBMITTED = ".govuk-panel__title"
     LINK_DELETE_DRAFT_ID = "link-delete-draft"
 
     def enter_name_or_reference_for_application(self, name):
-        element = self.driver.find_element_by_id(self.name_or_reference_input_id)
+        element = self.driver.find_element_by_id(self.NAME_OR_REFERENCE_INPUT_ID)
         element.clear()
         element.send_keys(name)
 
@@ -31,24 +25,23 @@ class ApplyForALicencePage(BasePage):
         functions.click_submit(self.driver)
 
     def click_export_licence(self, export_type):
-        logging.info(export_type)
         if export_type == "standard":
             return self.driver.find_element_by_css_selector(self.BUTTON_STANDARD_LICENCE).click()
         elif export_type == "open":
             return self.driver.find_element_by_css_selector(self.BUTTON_OPEN_LICENCE).click()
 
     def click_permanent_or_temporary_button(self, string):
-        self.driver.find_element_by_id(self.export_button + string).click()
+        self.driver.find_element_by_id(self.EXPORT_BUTTON + string).click()
 
     def click_export_licence_yes_or_no(self, string):
-        self.driver.find_element_by_id(self.export_licence_yes_or_no + string).click()
+        self.driver.find_element_by_id(self.EXPORT_LICENCE_YES_OR_NO + string).click()
 
     def get_text_of_success_message(self):
-        return self.driver.find_element_by_css_selector(self.success_message).text
+        return self.driver.find_element_by_css_selector(self.SUCCESS_MESSAGE).text
 
     def type_into_reference_number(self, string):
-        self.driver.find_element_by_id(self.reference_number).clear()
-        self.driver.find_element_by_id(self.reference_number).send_keys(string)
+        self.driver.find_element_by_id(self.REFERENCE_NUMBER).clear()
+        self.driver.find_element_by_id(self.REFERENCE_NUMBER).send_keys(string)
 
     def application_submitted_text(self):
-        return self.driver.find_element_by_css_selector(self.application_is_submitted).text
+        return self.driver.find_element_by_css_selector(self.APPLICATION_IS_SUBMITTED).text
