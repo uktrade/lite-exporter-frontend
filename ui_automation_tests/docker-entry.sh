795310a8359ac9769c7681c1a2afb8f9581d7046
Nick Philpott <nicholasp@kainos.com>
2019-11-19 12:47:15 +0000

    update

--- ui_automation_tests/docker-entry.sh
+++ ui_automation_tests/docker-entry.sh
@@ -6,9 +6,10 @@ parallel_option=''
 # run py.test ($@ to derive parameters from commandline)
 if [ $NO_OF_PARALLEL_RUNNERS -gt 1 ]
 then
-    parallel_option="-n=$NO_OF_PARALLEL_RUNNERS"
+    py.test -k "$TESTS_TO_RUN" "$parallel_option" --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
+else
+    py.test -k "$TESTS_TO_RUN" --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
 fi
-py.test -k "$TESTS_TO_RUN" "$parallel_option" --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
 pid="$!"
 
 # trap process id to stop script using Control+C
