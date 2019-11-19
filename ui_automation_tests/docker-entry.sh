b39ef1e016ad0acdeac3c3c05b0e22297ede40f6
Nick Philpott <nicholasp@kainos.com>
2019-11-19 12:54:36 +0000

    did a oops

--- ui_automation_tests/docker-entry.sh
+++ ui_automation_tests/docker-entry.sh
@@ -7,7 +7,7 @@ if [ $NO_OF_PARALLEL_RUNNERS -gt 1 ]
 then
     py.test -k "$TESTS_TO_RUN" -n="$NO_OF_PARALLEL_RUNNERS" --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
 else
-    py.test -k "$TESTS_TO_RUN" --dist=loadscope --ignore=core --alluredir=ui_automation_tests/allure-results &
+    py.test -k "$TESTS_TO_RUN" --ignore=core --alluredir=ui_automation_tests/allure-results &
 fi
 pid="$!"
 
