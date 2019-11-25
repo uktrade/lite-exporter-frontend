dd5be675de58525baec164d7e891b32612d1ce9e
cstobart <c.stobart@kainos.com>
2019-11-25 17:38:21 +0000

    Update urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -1,17 +1,6 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('applications/', include('applications.urls')),
-    path('apply-for-a-licence/', include('apply_for_a_licence.urls')),
-    path('auth/', include('auth.urls', namespace='authbroker')),
-    path('end-users/', include('end_users.urls')),
-    path('goods/', include('goods.urls')),
-    path('licences/', include('licences.urls')),
-    path('sites/', include('sites.urls')),
-    path('users/', include('users.urls')),
-    path('roles/', include('roles.urls')),
-    path('', include('hmrc.urls'))
     path("", include("core.urls")),
     path("applications/", include("applications.urls")),
     path("apply-for-a-licence/", include("apply_for_a_licence.urls")),
@@ -21,5 +10,6 @@ urlpatterns = [
     path("licences/", include("licences.urls")),
     path("sites/", include("sites.urls")),
     path("users/", include("users.urls")),
+    path('roles/', include("roles.urls")),
     path("", include("hmrc.urls")),
 ]
