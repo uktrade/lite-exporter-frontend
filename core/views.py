e7acb30be4b8112fb7341a27585363f42f752239
cstobart <c.stobart@kainos.com>
2019-11-27 15:59:29 +0000

    more content

--- core/views.py
+++ core/views.py
@@ -23,10 +23,10 @@ class Hub(TemplateView):
             title = strings.HUB_MANAGE_MY_USERS
         elif Permissions.ADMINISTER_SITES in user_permissions:
             manage_organisation_section_link = reverse_lazy("sites:sites")
-            title = strings.HUB_MANAGE_MY_USERS
+            title = strings.HUB_MANAGE_MY_SITES
         elif Permissions.EXPORTER_ADMINISTER_ROLES in user_permissions:
             manage_organisation_section_link = reverse_lazy("roles:roles")
-            title = strings.HUB_MANAGE_MY_USERS
+            title = strings.HUB_MANAGE_MY_ROLES
         else:
             manage_organisation_section_link = None
 
@@ -82,7 +82,7 @@ class Hub(TemplateView):
                     if permission in Permissions.MANAGE_ORGANISATION_PERMISSIONS:
                         number_permissions += 1
                 if number_permissions > 1:
-                    title = strings.HUB_MANAGE_MY_USERS
+                    title = strings.HUB_MANAGE_MY_ORGANISATION
                 sections[1].tiles.append(Tile(title, "", manage_organisation_section_link))
 
         context = {
--- roles/forms.py
+++ roles/forms.py
@@ -1,41 +1,42 @@
 from django.http import HttpRequest
 from django.urls import reverse_lazy
 
+from lite_content.lite_exporter_frontend import strings
 from lite_forms.components import Form, TextInput, Checkboxes, BackLink
 from roles.services import get_permissions
 
 
 def add_role(request: HttpRequest):
     return Form(
-        title="Junk string title",
-        description="Junk string description",
+        title=strings.ADD_ROLE_TITLE,
+        description=strings.ADD_ROLE_DESCRIPTION,
         questions=[
-            TextInput(title="What do you want to call the role?", name="name"),
+            TextInput(title=strings.ROLES_ADD_NAME, name="name"),
             Checkboxes(
                 name="permissions[]",
                 options=get_permissions(request, True),
-                title="What permissions should this role have?",
-                description="Select all permissions that apply.",
+                title=strings.ROLES_ADD_PERMISSIONS,
+                description=strings.ROLES_ADD_PERMISSIONS_DESCRIPTION,
             ),
         ],
-        back_link=BackLink("Back to Roles", reverse_lazy("roles:roles")),
-        default_button_name="Create",
+        back_link=BackLink(strings.ROLES_ADD_FORM_BACK_TO_ROLES, reverse_lazy("roles:roles")),
+        default_button_name=strings.ROLES_ADD_FORM_CREATE,
     )
 
 
 def edit_role(request: HttpRequest):
     return Form(
-        title="Junk string title",
-        description="Junk string description",
+        title=strings.EDIT_ROLE_TITLE,
+        description=strings.EDIT_ROLE_DESCRIPTION,
         questions=[
-            TextInput(title="What do you want to call the role?", name="name"),
+            TextInput(title=strings.ROLES_EDIT_NAME, name="name"),
             Checkboxes(
                 name="permissions[]",
                 options=get_permissions(request, True),
-                title="What permissions should this role have?",
-                description="Select all permissions that apply.",
+                title=strings.ROLES_EDIT_PERMISSIONS,
+                description=strings.ROLES_EDIT_PERMISSIONS_DESCRIPTION,
             ),
         ],
-        back_link=BackLink("Back to Roles", reverse_lazy("roles:roles")),
-        default_button_name="Save",
+        back_link=BackLink(strings.ROLES_EDIT_FORM_BACK_TO_ROLES, reverse_lazy("roles:roles")),
+        default_button_name=strings.ROLES_EDIT_FORM_CREATE,
     )
