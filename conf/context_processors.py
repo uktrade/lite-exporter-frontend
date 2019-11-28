5ecd85bde528a8737ddd22288676c52ce771f389
Jan Faracik <j.faracik@kainos.com>
2019-11-28 16:47:06 +0000

    Update context_processors.py

--- conf/context_processors.py
+++ conf/context_processors.py
@@ -8,7 +8,7 @@ def export_vars(request):
         "FEEDBACK_URL": env("FEEDBACK_URL"),
         "INTERNAL_URL": env("INTERNAL_FRONTEND_URL"),
         "GOOGLE_ANALYTICS_KEY": env("GOOGLE_ANALYTICS_KEY"),
-        'CURRENT_PATH': request.get_full_path(),
-        'CURRENT_PATH_WITHOUT_PARAMS': request.get_full_path().split('?')[0].split('#')[0]
+        "CURRENT_PATH": request.get_full_path(),
+        "CURRENT_PATH_WITHOUT_PARAMS": request.get_full_path().split("?")[0].split("#")[0],
     }
     return data
