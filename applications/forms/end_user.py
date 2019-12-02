c2f11a49c065f4d4ec98c6770d9c025fce6a5386
BenAndrew-Kainos <bena@kainos.com>
2019-12-02 14:45:05 +0000

    Refactor party form building to simplify

--- applications/forms/end_user.py
+++ applications/forms/end_user.py
@@ -8,43 +8,51 @@ from lite_forms.components import RadioButtons, Form, Option, TextArea, TextInpu
 from lite_forms.generators import confirm_form
 
 
-def third_parties_standard_form(application, opening_title):
-    return [
-        Form(
-            title=opening_title,
-            questions=[
-                RadioButtons(
-                    "sub_type",
-                    options=[
-                        Option("government", "A Government Organisation"),
-                        Option("commercial", "A Commercial Organisation"),
-                        Option("individual", "An Individual"),
-                        Option("other", "Other", show_or=True),
-                    ],
-                ),
-            ],
-            default_button_name="Continue",
-            back_link=back_to_task_list(application["id"]),
-        ),
-        Form(title="Enter the final recipient's name", questions=[TextInput("name"),], default_button_name="Continue"),
-        Form(
-            title="Enter the final recipient's web address (URL)",
-            questions=[TextInput("website", optional=True),],
-            default_button_name="Continue",
-        ),
-        Form(
-            title="Where's the final recipient based?",
-            questions=[
-                TextArea("address", "Address"),
-                country_question(countries=get_countries(None, True), prefix=""),
-            ],
-            default_button_name="Save and continue",
-        ),
-    ]
+def _party_type_form(application, title):
+    return Form(
+        title=title,
+        questions=[
+            RadioButtons(
+                "sub_type",
+                options=[
+                    Option("government", "A Government Organisation"),
+                    Option("commercial", "A Commercial Organisation"),
+                    Option("individual", "An Individual"),
+                    Option("other", "Other", show_or=True),
+                ],
+            ),
+        ],
+        default_button_name="Continue",
+        back_link=back_to_task_list(application["id"]),
+    )
+
+
+def _party_name_form():
+    return Form(
+        title="Enter the final recipient's name", questions=[TextInput("name"),], default_button_name="Continue"
+    )
+
+
+def _party_website_form():
+    return Form(
+        title="Enter the final recipient's web address (URL)",
+        questions=[TextInput("website", optional=True),],
+        default_button_name="Continue",
+    )
+
+
+def _party_address_form():
+    return Form(
+        title="Where's the final recipient based?",
+        questions=[TextArea("address", "Address"), country_question(countries=get_countries(None, True), prefix=""),],
+        default_button_name="Save and continue",
+    )
 
 
 def new_end_user_forms(application, title):
-    return FormGroup(third_parties_standard_form(application, title))
+    return FormGroup(
+        [_party_type_form(application, title), _party_name_form(), _party_website_form(), _party_address_form()]
+    )
 
 
 def attach_document_form(application_id, title, return_later_text, description_text=None):
--- applications/forms/third_party.py
+++ applications/forms/third_party.py
@@ -1,7 +1,7 @@
 from lite_forms.components import RadioButtons, Form, Option, FormGroup
 
-from applications.forms.end_user import third_parties_standard_form
 from lite_content.lite_exporter_frontend.applications import ThirdPartyForm
+from applications.forms.end_user import _party_name_form, _party_website_form, _party_address_form
 
 option_list = {
     "agent": ThirdPartyForm.Options.AGENT,
@@ -14,20 +14,24 @@ option_list = {
 }
 
 
+def _third_party_type_form(title, options):
+    return Form(title=title, questions=[RadioButtons("sub_type", options=options)], default_button_name="Continue",)
+
+
 def third_party_forms(application):
     if application["export_type"] and application["export_type"]["key"] == "permanent":
         form_options = option_list.copy()
         del form_options["additional_end_user"]
     else:
         form_options = option_list.copy()
-
-    third_party_form = third_parties_standard_form(application, ThirdPartyForm.TITLE)
     options = [Option(key, value) for key, value in form_options.items()]
     options.append(Option("other", "Other", show_or=True))
-    third_party_type = Form(
-        title=ThirdPartyForm.TITLE,
-        questions=[RadioButtons("sub_type", options=options)],
-        default_button_name="Continue",
+
+    return FormGroup(
+        [
+            _third_party_type_form(ThirdPartyForm.TITLE, options),
+            _party_name_form(),
+            _party_website_form(),
+            _party_address_form(),
+        ]
     )
-    third_party_form[0] = third_party_type
-    return FormGroup(third_party_form)
