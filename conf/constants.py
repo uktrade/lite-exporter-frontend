0fef2ff42cbf62faa58fee6b895f5ce50baaa8bc
stephanieboyd <54948803+stephanieboyd@users.noreply.github.com>
2019-12-02 16:03:17 +0000

    Lt 920 finalised cases (#222)
    * Prohibit adding case note when case in terminal status

--- conf/constants.py
+++ conf/constants.py
@@ -37,6 +37,7 @@ END_USER_ADVISORIES_URL = QUERIES_URL + "end-user-advisories/"
 
 # Static URLs
 STATIC_URL = "/static/"
+STATUS_PROPERTIES_URL = STATIC_URL + "statuses/properties/"
 UNITS_URL = STATIC_URL + "units/"
 COUNTRIES_URL = STATIC_URL + "countries/"
 CONTROL_LIST_ENTRIES_URL = STATIC_URL + "control-list-entries/"
@@ -45,9 +46,6 @@ CONTROL_LIST_ENTRIES_URL = STATIC_URL + "control-list-entries/"
 STANDARD_LICENCE = "standard_licence"
 OPEN_LICENCE = "open_licence"
 HMRC_QUERY = "hmrc_query"
-
-# Case statuses
-READ_ONLY_STATUSES = ["finalised", "under final review", "under review", "withdrawn"]
 
 APPLICANT_EDITING = "applicant_editing"
 
