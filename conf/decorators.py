51b03f5ebf3911d0c8609dcb28bb805d04f1891a
Mark Spiers <marksp@kainos.com>
2019-12-06 16:04:06 +0000

    initial implementation and goods complete

---
+++ conf/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+from lite_forms.generators import error_page
+
+
+def acceptable_status(statuses: [int]):
+    """ Check if an application is in an editable state. """
+
+    def decorator(func):
+        @wraps(func)
+        def inner(request, *args, **kwargs):
+            data = func(request, *args, **kwargs)
+
+            if data[1] not in statuses:
+                raise Exception("Status code was not expected during '" + str(func.__name__) + "()'")
+
+            return data
+
+        return inner
+
+    return decorator
--- goods/services.py
+++ goods/services.py
@@ -3,9 +3,11 @@ from urllib.parse import urlencode
 
 from conf.client import get, post, put, delete
 from conf.constants import GOODS_URL, DOCUMENTS_URL, CONTROL_LIST_CLASSIFICATIONS_URL
+from conf.decorators import acceptable_status
 from core.helpers import remove_prefix
 
 
+@acceptable_status([200])
 def get_goods(request, params=None):
     if params:
         query_params = urlencode(params)
@@ -16,11 +18,13 @@ def get_goods(request, params=None):
     return data.json().get("goods"), data.status_code
 
 
+@acceptable_status([200, 404])
 def get_good(request, pk):
     data = get(request, GOODS_URL + pk)
     return data.json().get("good"), data.status_code
 
 
+@acceptable_status([201, 404])
 def post_goods(request, json):
     if json.get("good_description", False) or json.get("good_description") == "":
         post_data = remove_prefix(json, "good_")
@@ -30,6 +34,7 @@ def post_goods(request, json):
     return data.json(), data.status_code
 
 
+@acceptable_status([200, 404])
 def validate_good(request, json):
     if json.get("good_description", False) or json.get("good_description") == "":
         post_data = remove_prefix(json, "good_")
@@ -40,37 +45,44 @@ def validate_good(request, json):
     return data
 
 
+@acceptable_status([200, 400, 404])
 def update_good(request, pk, json):
     data = put(request, GOODS_URL + pk + "/", json)
     return data.json(), data.status_code
 
 
+@acceptable_status([200, 400, 404])
 def delete_good(request, pk):
     data = delete(request, GOODS_URL + pk)
     return data.json(), data.status_code
 
 
+@acceptable_status([201, 404])
 def raise_clc_query(request, json):
     data = post(request, CONTROL_LIST_CLASSIFICATIONS_URL, json)
     return data.json(), data.status_code
 
 
 # Documents
+@acceptable_status([200, 400, 404])
 def get_good_document(request, pk, doc_pk):
     data = get(request, GOODS_URL + pk + DOCUMENTS_URL + doc_pk)
     return data.json().get("document") if data.status_code == HTTPStatus.OK else None
 
 
+@acceptable_status([200, 404])
 def get_good_documents(request, pk):
     data = get(request, GOODS_URL + pk + DOCUMENTS_URL)
     return data.json().get("documents") if data.status_code == HTTPStatus.OK else None
 
 
+@acceptable_status([200, 404])
 def post_good_documents(request, pk, json):
     data = post(request, GOODS_URL + pk + DOCUMENTS_URL, json)
     return data.json(), data.status_code
 
 
+@acceptable_status([200, 400, 404])
 def delete_good_document(request, pk, doc_pk):
     data = delete(request, GOODS_URL + pk + DOCUMENTS_URL + doc_pk)
     return data.json(), data.status_code
