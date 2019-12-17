8aea1690ef318103a6743405eb080b0ab591166f
Andy Taylor <a.taylor@kainos.com>
2019-12-17 17:15:36 +0000

    WIP: added serializer method for getting notifications on eua queries

--- end_users/views.py
+++ end_users/views.py
@@ -29,12 +29,10 @@ from end_users.services import get_end_user_advisories, post_end_user_advisories
 class EndUsersList(TemplateView):
     def get(self, request, **kwargs):
         end_users = get_end_user_advisories(request)
-        notifications = get_notifications(request, unviewed=True)
 
         context = {
             "title": "End User Advisories",
             "end_users": end_users,
-            "notifications": group_notifications(notifications),
         }
         return render(request, "end_users/end_users.html", context)
 
@@ -136,28 +134,10 @@ class EndUserDetail(TemplateView):
         return super(EndUserDetail, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, **kwargs):
-        notifications = get_notifications(request, unviewed=True)
-        case_note_notifications = len(
-            [
-                x
-                for x in notifications
-                if str(x["parent"]) == self.end_user_advisory_id and x["object_type"] == "case_note"
-            ]
-        )
-        ecju_query_notifications = len(
-            [
-                x
-                for x in notifications
-                if str(x["parent"]) == self.end_user_advisory_id and x["object_type"] == "ecju_query"
-            ]
-        )
-
         context = {
             "title": "End User Advisory",
             "case_id": self.case_id,
             "end_user_advisory": self.end_user_advisory,
-            "case_note_notifications": case_note_notifications,
-            "ecju_query_notifications": ecju_query_notifications,
             "type": self.view_type,
         }
 
