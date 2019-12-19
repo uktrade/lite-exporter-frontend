e106af2b36c098198a9550b6d4a30c18cf3f454a
Jan Faracik <j.faracik@kainos.com>
2019-12-19 15:58:26 +0000

    bosh :100:

--- ui_automation_tests/conftest.py
+++ ui_automation_tests/conftest.py
@@ -47,7 +47,6 @@ from ui_automation_tests.shared.fixtures.add_a_generated_document import add_a_g
 from ui_automation_tests.shared.fixtures.driver import driver  # noqa
 from ui_automation_tests.shared.fixtures.core import (  # noqa
     context,
-    invalid_username,
     exporter_info,
     internal_info,
     seed_data_config,
