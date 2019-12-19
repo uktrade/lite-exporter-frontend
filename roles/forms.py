c9583b0314b799138ceb079834b5c7385ca8e842
BenAndrew-Kainos <bena@kainos.com>
2019-12-19 14:57:59 +0000

    AddRoleForm strings

--- roles/forms.py
+++ roles/forms.py
@@ -8,35 +8,35 @@ from roles.services import get_permissions
 
 def add_role(request: HttpRequest):
     return Form(
-        title=strings.ADD_ROLE_TITLE,
-        description=strings.ADD_ROLE_DESCRIPTION,
+        title=strings.roles.AddRoleForm.TITLE,
+        description=strings.roles.AddRoleForm.DESCRIPTION,
         questions=[
-            TextInput(title=strings.ROLES_ADD_NAME, name="name"),
+            TextInput(title=strings.roles.AddRoleForm.ADD_NAME, name="name"),
             Checkboxes(
                 name="permissions[]",
                 options=get_permissions(request, True),
-                title=strings.ROLES_ADD_PERMISSIONS,
-                description=strings.ROLES_ADD_PERMISSIONS_DESCRIPTION,
+                title=strings.roles.AddRoleForm.ADD_PERMISSIONS,
+                description=strings.roles.AddRoleForm.ADD_PERMISSIONS_DESCRIPTION,
             ),
         ],
-        back_link=BackLink(strings.ROLES_ADD_FORM_BACK_TO_ROLES, reverse_lazy("roles:roles")),
-        default_button_name=strings.ROLES_ADD_FORM_CREATE,
+        back_link=BackLink(strings.roles.AddRoleForm.ADD_FORM_BACK_TO_ROLES, reverse_lazy("roles:roles")),
+        default_button_name=strings.roles.AddRoleForm.ADD_FORM_CREATE,
     )
 
 
 def edit_role(request: HttpRequest):
     return Form(
-        title=strings.EDIT_ROLE_TITLE,
-        description=strings.EDIT_ROLE_DESCRIPTION,
+        title=strings.roles.EditRoleForm.TITLE,
+        description=strings.roles.EditRoleForm.DESCRIPTION,
         questions=[
-            TextInput(title=strings.ROLES_EDIT_NAME, name="name"),
+            TextInput(title=strings.roles.EditRoleForm.EDIT_NAME, name="name"),
             Checkboxes(
                 name="permissions[]",
                 options=get_permissions(request, True),
-                title=strings.ROLES_EDIT_PERMISSIONS,
-                description=strings.ROLES_EDIT_PERMISSIONS_DESCRIPTION,
+                title=strings.roles.EditRoleForm.EDIT_PERMISSIONS,
+                description=strings.roles.EditRoleForm.EDIT_PERMISSIONS_DESCRIPTION,
             ),
         ],
-        back_link=BackLink(strings.ROLES_EDIT_FORM_BACK_TO_ROLES, reverse_lazy("roles:roles")),
-        default_button_name=strings.ROLES_EDIT_FORM_CREATE,
+        back_link=BackLink(strings.roles.EditRoleForm.EDIT_FORM_BACK_TO_ROLES, reverse_lazy("roles:roles")),
+        default_button_name=strings.roles.EditRoleForm.EDIT_FORM_CREATE,
     )
