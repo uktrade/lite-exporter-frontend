74965e97fa5da5d2b007c35f177d8391317b2905
Jan Faracik <j.faracik@kainos.com>
2019-12-23 11:20:03 +0000

    Cleanup

--- applications/forms/goods_types.py
+++ applications/forms/goods_types.py
@@ -7,30 +7,26 @@ from core.services import get_control_list_entries
 
 def goods_type_form():
     return Form(
-        title=get_string("good_types.overview.title"),
+        title="Describe your products",
         questions=[
-            TextArea(title="Description", name="description", extras={"max_length": 2000,}),
+            TextArea(title="Description", name="description", extras={"max_length": 2000, }),
             RadioButtons(
                 title="Are your products controlled?",
-                description="Products that aren't on the <a class='govuk-link' target='_blank' "
-                "href='https://permissions-finder.service.trade.gov.uk/'>control list</a> "
-                "may be affected by military end use controls, current trade sanctions and embargoes or weapons of "
-                "mass destruction controls. If your products aren't subject to any controls, you'll get a no licence "
-                "required (NLR) document from ECJU.",
+                description="Products that aren't on the [control list](https://permissions-finder.service.trade.gov.uk/) "
+                            "may be affected by military end use controls, current trade sanctions and embargoes or weapons of "
+                            "mass destruction controls. If your products aren't subject to any controls, you'll get a no licence "
+                            "required (NLR) document from ECJU.",
                 name="is_good_controlled",
                 options=[Option(key="yes", value="Yes", show_pane="pane_control_code"), Option(key="no", value="No")],
                 classes=["govuk-radios--inline"],
             ),
             control_list_entry_question(
                 control_list_entries=get_control_list_entries(None, convert_to_options=True),
-                title="Control list classification",
-                description="For example, ML1a.",
                 name="control_code",
                 inset_text=False,
             ),
             RadioButtons(
                 title="Will the products be incorporated into other products",
-                description="",
                 name="is_good_end_product",
                 options=[Option(key="yes", value="Yes"), Option(key="no", value="No")],
                 classes=["govuk-radios--inline"],
