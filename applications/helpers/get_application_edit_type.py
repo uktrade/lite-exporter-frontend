36a4df695802acef686adad7e21ff88c430ad0bd
Jan Faracik <j.faracik@kainos.com>
2019-12-23 11:02:38 +0000

    Update get_application_edit_type.py

--- applications/helpers/get_application_edit_type.py
+++ applications/helpers/get_application_edit_type.py
@@ -13,5 +13,9 @@ def get_application_edit_type(application: dict):
     if application["status"]:
         is_editing = application["status"]["key"] == "submitted" or application["status"]["key"] == APPLICANT_EDITING
         if is_editing:
-            return ApplicationEditTypes.MINOR_EDIT if application["status"]["key"] == "submitted" else ApplicationEditTypes.MAJOR_EDIT
+            return (
+                ApplicationEditTypes.MINOR_EDIT
+                if application["status"]["key"] == "submitted"
+                else ApplicationEditTypes.MAJOR_EDIT
+            )
     return None
