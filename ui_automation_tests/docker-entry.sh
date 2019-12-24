f9f9b0fb6e46f8412ee608e91abbba60eb2a3b6e
Nick Philpott <nicholasp@kainos.com>
2019-12-24 13:32:33 +0000

    run smoke first

--- ui_automation_tests/docker-entry.sh
+++ ui_automation_tests/docker-entry.sh
@@ -3,11 +3,14 @@
 set -x
 
 # run py.test ($@ to derive parameters from commandline)
-if [ $NO_OF_PARALLEL_RUNNERS -gt 1 ]
+if [ -z $TESTS_TO_RUN ]
 then
-    py.test -k "$TESTS_TO_RUN" -n="$NO_OF_PARALLEL_RUNNERS" --dist=loadscope  --reruns 1 --ignore=core --alluredir=ui_automation_tests/allure-results &
-else
     py.test -k "$TESTS_TO_RUN"  --reruns 1 --ignore=core --alluredir=ui_automation_tests/allure-results &
+else
+    py.test -k "smoke"  --reruns 1 --ignore=core --alluredir=ui_automation_tests/allure-results &
+    if [ $? -eq 0 ]
+    py.test -k "regression"  --reruns 1 --ignore=core --alluredir=ui_automation_tests/allure-results &
+    fi
 fi
 pid="$!"
 
