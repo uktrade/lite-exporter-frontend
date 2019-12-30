4f18c9821b36044de6e6250afb666b423543f4c7
Nick Philpott <nicholasp@kainos.com>
2019-12-30 14:17:22 +0000

    rename

--- ui_automation_tests/step_defs/test_menu.py
+++ ui_automation_tests/step_defs/test_menu.py
@@ -5,8 +5,8 @@ from pytest_bdd import scenarios, then, when
 scenarios("../features/menu.feature", strict_gherkin=False)
 
 
-@then("I get a 200")
-def success(driver):
+@then("I get a 200 and log out link is displayed")
+def log_out_link_displayed_and_success_status_code(driver):
     conn = urllib.request.urlopen(driver.current_url)
     assert conn.getcode() == 200
     assert driver.find_element_by_id(
