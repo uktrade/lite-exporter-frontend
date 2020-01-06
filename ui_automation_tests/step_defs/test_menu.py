7a73d1ad3302614cea88180206cb55b57b776574
BenAndrew-Kainos <bena@kainos.com>
2020-01-06 14:13:38 +0000

    Fix test menu (remove redundant urllib code)

--- ui_automation_tests/step_defs/test_menu.py
+++ ui_automation_tests/step_defs/test_menu.py
@@ -1,5 +1,3 @@
-import urllib.request
-
 from pytest_bdd import scenarios, then, when
 
 scenarios("../features/menu.feature", strict_gherkin=False)
@@ -7,8 +5,6 @@ scenarios("../features/menu.feature", strict_gherkin=False)
 
 @then("I get a 200 and log out link is displayed")
 def log_out_link_displayed_and_success_status_code(driver):
-    conn = urllib.request.urlopen(driver.current_url)
-    assert conn.getcode() == 200
     assert driver.find_element_by_id(
         "link-sign-out"
     ).is_displayed(), "Log out button is not displayed. User may have the service unavailable screen."
