29de7117327668632124a12505f91657a5040f9f
Jan Faracik <j.faracik@kainos.com>
2020-01-08 17:24:45 +0000

    Fix

--- ui_automation_tests/step_defs/test_edit_open_application.py
+++ ui_automation_tests/step_defs/test_edit_open_application.py
@@ -1,28 +1,9 @@
-from pytest_bdd import when, scenarios, then, given
-
-from pages.generic_application.task_list import GenericApplicationTaskListPage
-from pages.open_application.goods_types import OpenApplicationGoodsTypesPage
-
-scenarios("../features/edit_open_application.feature", strict_gherkin=False)
-
-
-@given("I create an open application via api")
-def open_application_exists(apply_for_open_application):
-    pass
-
-
-@when("I remove a good type from the application")
-def i_remove_a_good_from_the_application(driver):
-    remove_good_link = GenericApplicationTaskListPage(driver).find_remove_goods_type_link()
-    driver.execute_script("arguments[0].click();", remove_good_link)
-
-
-@then("no goods types are left on the application")
-def no_goods_types_are_left_on_the_application(driver):
-    assert (OpenApplicationGoodsTypesPage(driver).find_remove_goods_type_link(), None)
-
-
-@when("I click on open goods tile")  # noqa
-def click_my_goods_link(driver):  # noqa
-    exporter_hub = GenericApplicationTaskListPage(driver)
-    exporter_hub.click_goods_type_link()
+# from pytest_bdd import when, scenarios, then, given
+#
+# from pages.generic_application.task_list import GenericApplicationTaskListPage
+# from pages.open_application.goods_types import OpenApplicationGoodsTypesPage
+#
+# scenarios("../features/edit_open_application.feature", strict_gherkin=False)
+#
+#
+#
--- ui_automation_tests/step_defs/test_open_application.py
+++ ui_automation_tests/step_defs/test_open_application.py
@@ -1,4 +1,4 @@
-from pytest_bdd import scenarios, when, then, parsers
+from pytest_bdd import scenarios, when, then, parsers, given
 
 import shared.tools.helpers as utils
 from conftest import (
@@ -7,17 +7,18 @@ from conftest import (
     enter_application_name,
     enter_permanent_or_temporary,
 )
-from pages.add_goods_page import AddGoodPage
+from pages.generic_application.task_list import GenericApplicationTaskListPage
 from pages.open_application.add_goods_type import OpenApplicationAddGoodsType
 from pages.open_application.countries import OpenApplicationCountriesPage
-from pages.open_application.goods_types import OpenApplicationGoodsTypesPage
-from pages.generic_application.task_list import GenericApplicationTaskListPage
 from pages.open_application.goods_countries_page import GoodsCountriesPage
+from pages.open_application.goods_types import OpenApplicationGoodsTypesPage
 from pages.open_application.task_list import OpenApplicationTaskListPage
 from pages.shared import Shared
 from shared import functions
 
-scenarios("../features/submit_open_application.feature", strict_gherkin=False)
+scenarios("../features/submit_open_application.feature",
+          "../features/edit_open_application.feature",
+          strict_gherkin=False)
 
 
 @then("I see good types error messages")
@@ -146,3 +147,26 @@ def add_new_goods_type(driver, description, controlled, control_code, incorporat
     context.control_code = control_code
 
     functions.click_submit(driver)
+
+
+# Edit
+@given("I create an open application via api")
+def open_application_exists(apply_for_open_application):
+    pass
+
+
+@when("I remove a good type from the application")
+def i_remove_a_good_from_the_application(driver):
+    remove_good_link = GenericApplicationTaskListPage(driver).find_remove_goods_type_link()
+    driver.execute_script("arguments[0].click();", remove_good_link)
+
+
+@then("no goods types are left on the application")
+def no_goods_types_are_left_on_the_application(driver):
+    assert (OpenApplicationGoodsTypesPage(driver).find_remove_goods_type_link(), None)
+
+
+@when("I click on open goods tile")  # noqa
+def click_my_goods_link(driver):  # noqa
+    exporter_hub = GenericApplicationTaskListPage(driver)
+    exporter_hub.click_goods_type_link()
