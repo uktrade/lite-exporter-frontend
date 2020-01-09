6c582801f808f123746ac48eac062df09dbf7b6d
Jan Faracik <j.faracik@kainos.com>
2020-01-09 16:38:41 +0000

    Update goods.py

--- ui_automation_tests/pages/standard_application/goods.py
+++ ui_automation_tests/pages/standard_application/goods.py
@@ -7,20 +7,15 @@ class StandardApplicationGoodsPage(BasePage):
     BUTTON_ADD_NEW_GOOD_ID = "button-add-new-good"
     BUTTON_ADD_PREEXISTING_GOOD_ID = "button-add-preexisting-good"
     SPAN_GOODS_TOTAL_VALUE = "span-goods-total-value"
-    BUTTON_ADD_NEW_GOOD = "add-new"  # ID
-    quantity_field = "quantity"  # ID
-    unit_dropdown = "unit"  # ID
-    value_field = "value"  # ID
-    filter_description_search_box = "description"  # ID
-    filter_part_number_search_box = "part_number"  # ID
-    filter_control_rating_search_box = "control_rating"  # ID
-    filter_button = "button-apply-filters"  # ID
-    card_label = ".lite-card .govuk-label"
-    part_number = "good-part-number"
-    show_filters_link = "show-filters-link"  # ID
-    description = "good-description"
-    control_code = "good-control_code"
-    good_entry = ".govuk-table__body .govuk-table__row"
+    SEARCH_DESCRIPTION_ID = "description"
+    SEARCH_PART_NUMBER_ID = "part_number"
+    SEARCH_CONTROL_LIST_ENTRY_ID = "control_rating"
+    BUTTON_FILTER_ID = "button-apply-filters"
+    LINK_SHOW_FILTERS_ID = "show-filters-link"
+    PART_NUMBER = "good-part-number"
+    DESCRIPTION = "good-description"
+    CONTROL_LIST_ENTRY = "good-control_code"
+    GOOD_ENTRY = ".govuk-table__body .govuk-table__row"
 
     def click_add_new_good_button(self):
         self.driver.find_element_by_id(self.BUTTON_ADD_NEW_GOOD_ID).click()
@@ -34,31 +29,27 @@ class StandardApplicationGoodsPage(BasePage):
     def get_goods_total_value(self):
         return self.driver.find_element_by_id(self.SPAN_GOODS_TOTAL_VALUE).text
 
-    # old
     def type_into_filter_description_search_box_and_filter(self, value):
-        self.driver.find_element_by_id(self.show_filters_link).click()
-        self.driver.find_element_by_id(self.filter_description_search_box).send_keys(value)
-        self.driver.find_element_by_id(self.filter_button).click()
+        self.driver.find_element_by_id(self.LINK_SHOW_FILTERS_ID).click()
+        self.driver.find_element_by_id(self.SEARCH_DESCRIPTION_ID).send_keys(value)
+        self.driver.find_element_by_id(self.BUTTON_FILTER_ID).click()
 
     def type_into_filter_part_number_search_box_and_filter(self, value):
-        self.driver.find_element_by_id(self.filter_part_number_search_box).send_keys(value)
-        self.driver.find_element_by_id(self.filter_button).click()
+        self.driver.find_element_by_id(self.SEARCH_PART_NUMBER_ID).send_keys(value)
+        self.driver.find_element_by_id(self.BUTTON_FILTER_ID).click()
 
     def type_into_filter_control_rating_search_box_and_filter(self, value):
-        self.driver.find_element_by_id(self.filter_control_rating_search_box).send_keys(value)
-        self.driver.find_element_by_id(self.filter_button).click()
+        self.driver.find_element_by_id(self.SEARCH_CONTROL_LIST_ENTRY_ID).send_keys(value)
+        self.driver.find_element_by_id(self.BUTTON_FILTER_ID).click()
 
     def get_good_descriptions(self):
-        return self.driver.find_elements_by_id(self.description)
+        return self.driver.find_elements_by_id(self.DESCRIPTION)
 
     def get_good_part_numbers(self):
-        return self.driver.find_elements_by_id(self.part_number)
+        return self.driver.find_elements_by_id(self.PART_NUMBER)
 
     def get_good_control_codes(self):
-        return self.driver.find_elements_by_id(self.control_code)
-
-    def click_add_new_good_button(self):
-        return self.driver.find_element_by_id(self.BUTTON_ADD_NEW_GOOD).click()
+        return self.driver.find_elements_by_id(self.CONTROL_LIST_ENTRY)
 
     def get_goods_count(self):
-        return len(self.driver.find_elements_by_css_selector(self.good_entry))
+        return len(self.driver.find_elements_by_css_selector(self.GOOD_ENTRY))
