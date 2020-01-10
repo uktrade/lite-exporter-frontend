7beab28b3141dd764fe6c6f101755aad37ac6678
BenAndrew-Kainos <bena@kainos.com>
2020-01-10 16:53:00 +0000

    Rename AddEndUserPages fields

--- ui_automation_tests/pages/add_end_user_pages.py
+++ ui_automation_tests/pages/add_end_user_pages.py
@@ -3,10 +3,10 @@ from shared import functions
 
 
 class AddEndUserPages(BasePage):
-    NAME_FIELD = "name"  # ID
-    ADDRESS_FIELD = "address"  # ID
-    COUNTRY_FIELD = "country"  # ID
-    WEBSITE_FIELD = "website"  # ID
+    INPUT_NAME_ID = "name"  # ID
+    INPUT_ADDRESS_ID = "address"  # ID
+    INPUT_COUNTRY_ID = "country"  # ID
+    INPUT_WEBSITE_ID = "website"  # ID
 
     FILTER_LINK = "show-filters-link"  # ID
     FILTER_NAME_FIELD = "name"  # ID
@@ -27,35 +27,35 @@ class AddEndUserPages(BasePage):
         functions.click_submit(self.driver)
 
     def enter_name(self, name):
-        name_tb = self.driver.find_element_by_id(self.NAME_FIELD)
+        name_tb = self.driver.find_element_by_id(self.INPUT_NAME_ID)
         name_tb.clear()
         name_tb.send_keys(name)
 
     def get_name(self):
-        return self.driver.find_element_by_id(self.NAME_FIELD).get_attribute("value")
+        return self.driver.find_element_by_id(self.INPUT_NAME_ID).get_attribute("value")
 
     def enter_address(self, address):
-        address_tb = self.driver.find_element_by_id(self.ADDRESS_FIELD)
+        address_tb = self.driver.find_element_by_id(self.INPUT_ADDRESS_ID)
         address_tb.clear()
         address_tb.send_keys(address)
 
     def get_address(self):
-        return self.driver.find_element_by_id(self.ADDRESS_FIELD).get_attribute("value")
+        return self.driver.find_element_by_id(self.INPUT_ADDRESS_ID).get_attribute("value")
 
     def enter_website(self, website):
-        address_tb = self.driver.find_element_by_id(self.WEBSITE_FIELD)
+        address_tb = self.driver.find_element_by_id(self.INPUT_WEBSITE_ID)
         address_tb.clear()
         address_tb.send_keys(website)
 
     def get_website(self):
-        return self.driver.find_element_by_id(self.WEBSITE_FIELD).get_attribute("value")
+        return self.driver.find_element_by_id(self.INPUT_WEBSITE_ID).get_attribute("value")
 
     def enter_country(self, country):
-        country_tb = self.driver.find_element_by_id(self.COUNTRY_FIELD)
+        country_tb = self.driver.find_element_by_id(self.INPUT_COUNTRY_ID)
         country_tb.send_keys(country)
 
     def get_country(self):
-        return self.driver.find_element_by_id(self.COUNTRY_FIELD).get_attribute("value")
+        return self.driver.find_element_by_id(self.INPUT_COUNTRY_ID).get_attribute("value")
 
     def select_type(self, string):
         self.driver.find_element_by_id(self.TYPE_CHOICES + string).click()
