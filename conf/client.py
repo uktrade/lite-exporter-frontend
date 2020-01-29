452d1acc91295f4736ffdad304bec4eddea50f54
BenAndrew-Kainos <bena@kainos.com>
2020-01-29 16:39:07 +0000

    Read file in chunks to reduce memory usage

--- conf/client.py
+++ conf/client.py
@@ -1,8 +1,6 @@
-import mimetypes
-
 import requests
 
-from conf.settings import env
+from conf.settings import env, STREAMING_CHUNK_SIZE
 
 
 def get(request, appended_address):
@@ -57,10 +55,21 @@ def delete(request, appended_address):
     )
 
 
+def read_in_chunks(file):
+    """
+    Lazy function (generator) to read a file piece by piece.
+    """
+    while True:
+        data = file.read(STREAMING_CHUNK_SIZE)
+        if not data:
+            break
+        yield data
+
+
 def post_file(request, appended_address, file):
     return requests.post(
         env("LITE_API_URL") + appended_address,
-        data={"file": file.file.read()},
+        data={"file": read_in_chunks(file)},
         headers={
             "Content-Disposition": f"attachment; filename={file.name}",
             "EXPORTER-USER-TOKEN": str(request.user.user_token),
