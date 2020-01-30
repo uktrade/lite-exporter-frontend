431271992a9d2e835d45e229fa614b9b379c6dd6
Jan Faracik <j.faracik@kainos.com>
2020-01-30 15:29:17 +0000

    Conditional

--- applications/forms/goods_types.py
+++ applications/forms/goods_types.py
@@ -11,34 +11,37 @@ def goods_type_form(application_type: str):
         title=CreateGoodsTypeForm.TITLE,
         description=CreateGoodsTypeForm.DESCRIPTION,
         questions=[
-            TextArea(title=CreateGoodsTypeForm.Description.TITLE, name="description", extras={"max_length": 2000,}),
-            RadioButtons(
-                title=CreateGoodsTypeForm.IsControlled.TITLE,
-                description=CreateGoodsTypeForm.IsControlled.DESCRIPTION,
-                name="is_good_controlled",
-                options=[
-                    Option(key="yes", value=CreateGoodsTypeForm.IsControlled.YES, show_pane="pane_control_code"),
-                    Option(key="no", value=CreateGoodsTypeForm.IsControlled.NO),
-                ],
-                classes=["govuk-radios--inline"],
-            ),
-            control_list_entry_question(
-                control_list_entries=get_control_list_entries(None, convert_to_options=True),
-                name="control_code",
-                inset_text=False,
-            ),
-            conditional(
+            TextArea(title=CreateGoodsTypeForm.Description.TITLE, name="description", extras={"max_length": 2000, }),
+            *conditional(
                 application_type != HMRC_QUERY,
-                RadioButtons(
-                    title=CreateGoodsTypeForm.IsIncorporated.TITLE,
-                    description=CreateGoodsTypeForm.IsIncorporated.DESCRIPTION,
-                    name="is_good_incorporated",
-                    options=[
-                        Option(key=True, value=CreateGoodsTypeForm.IsIncorporated.YES),
-                        Option(key=False, value=CreateGoodsTypeForm.IsIncorporated.NO),
-                    ],
-                    classes=["govuk-radios--inline"],
-                ),
+                [
+                    RadioButtons(
+                        title=CreateGoodsTypeForm.IsControlled.TITLE,
+                        description=CreateGoodsTypeForm.IsControlled.DESCRIPTION,
+                        name="is_good_controlled",
+                        options=[
+                            Option(key="yes", value=CreateGoodsTypeForm.IsControlled.YES,
+                                   show_pane="pane_control_code"),
+                            Option(key="no", value=CreateGoodsTypeForm.IsControlled.NO),
+                        ],
+                        classes=["govuk-radios--inline"],
+                    ),
+                    control_list_entry_question(
+                        control_list_entries=get_control_list_entries(None, convert_to_options=True),
+                        name="control_code",
+                        inset_text=False,
+                    ),
+                    RadioButtons(
+                        title=CreateGoodsTypeForm.IsIncorporated.TITLE,
+                        description=CreateGoodsTypeForm.IsIncorporated.DESCRIPTION,
+                        name="is_good_incorporated",
+                        options=[
+                            Option(key=True, value=CreateGoodsTypeForm.IsIncorporated.YES),
+                            Option(key=False, value=CreateGoodsTypeForm.IsIncorporated.NO),
+                        ],
+                        classes=["govuk-radios--inline"],
+                    ),
+                ]
             ),
         ],
     )
