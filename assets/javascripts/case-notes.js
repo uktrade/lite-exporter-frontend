e952c9138d19400aaf9812a2319b044a72d3c9e7
Jan Faracik <j.faracik@kainos.com>
2020-02-04 16:09:32 +0000

    initial

--- assets/javascripts/case-notes.js
+++ assets/javascripts/case-notes.js
@@ -2,52 +2,40 @@ const MIN_LENGTH = 1;
 const VISIBLE_LENGTH = 1200;
 const MAX_LENGTH = 2200;
 
-$(".lite-expandable-textarea-controls").addClass("lite-expandable-textarea-controls--hidden");
-$(".case_note-warning").addClass("govuk-hint--hidden");
+disableButton($("#button-case-note-post"));
+disableLink($("#link-case-note-cancel"));
 
-setTimeout(
-	function() {
-		$(".lite-expandable-textarea").removeClass("lite-case-note-no-transition");
-	}, 400);
-
-$("#case_note").on('input propertychange paste', function() {
-	if ($(this).val() != '') {
-		$(".lite-expandable-textarea-controls").removeClass("lite-expandable-textarea-controls--hidden");
+$("#input-case-note").on("input propertychange paste", function() {
+	if ($(this).val().length > VISIBLE_LENGTH) {
+		$("#input-case-note-warning").text("You have " + (MAX_LENGTH - $(this).val().length) + " character" + pluralize(MAX_LENGTH - $(this).val().length) + " remaining");
 	} else {
-		$(".lite-expandable-textarea-controls").addClass("lite-expandable-textarea-controls--hidden");
+		$("#input-case-note-warning").text("You can enter up to " + MAX_LENGTH + " characters");
 	}
 
-	if ($(this).val().length > VISIBLE_LENGTH) {
-		$("#case_note-warning").text("You have " + (MAX_LENGTH - $(this).val().length) + " character" + pluralize(MAX_LENGTH - $(this).val().length) + " remaining");
+	if ($(this).val().length == 0) {
+		disableLink($("#link-case-note-cancel"));
 	} else {
-		$("#case_note-warning").text("You can enter up to " + MAX_LENGTH + " characters");
+		enableLink($("#link-case-note-cancel"));
 	}
 
 	if ($(this).val().length <= MIN_LENGTH) {
-		$("#button-post-note").addClass("govuk-button--disabled");
-		$("#button-post-note").attr("disabled", true);
+		disableButton($("#button-case-note-post"));
 	}
 
 	if ($(this).val().length > MAX_LENGTH) {
-		$("#case_note-warning").removeClass("govuk-hint");
-		$("#case_note-warning").addClass("govuk-error-message");
-		$("#case_note-warning").text("You have " + ($(this).val().length - MAX_LENGTH) + " character" + pluralize($(this).val().length - MAX_LENGTH) + " too many");
-		$("#button-post-note").addClass("govuk-button--disabled");
-		$("#button-post-note").attr("disabled", true);
-		$(".lite-expandable-textarea").addClass("lite-expandable-textarea--warning");
+		$("#input-case-note-warning").text("You have " + ($(this).val().length - MAX_LENGTH) + " character" + pluralize($(this).val().length - MAX_LENGTH) + " too many");
+		$("#input-case-note").parent().addClass("lite-case-note__container--error");
+		disableButton($("#button-case-note-post"));
 	} else {
-		$("#case_note-warning").removeClass("govuk-error-message");
-		$("#case_note-warning").addClass("govuk-hint");
-		$(".lite-expandable-textarea").removeClass("lite-expandable-textarea--warning");
+		$("#input-case-note").parent().removeClass("lite-case-note__container--error");
 		if ($(this).val().length > MIN_LENGTH) {
-			$("#button-post-note").removeClass("govuk-button--disabled");
-			$("#button-post-note").attr("disabled", false);
+			enableButton($("#button-case-note-post"));
 		}
 	}
 });
 
-$("#case-note-cancel-button").on("click", function() {
-	$("#case_note").val("");
-	$("#case_note").trigger("input");
+$("#link-case-note-cancel").on("click", function() {
+	$("#input-case-note").val("");
+	$("#input-case-note").trigger("input");
 	return false;
 });
