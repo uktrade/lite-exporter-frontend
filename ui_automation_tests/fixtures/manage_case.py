ec9d19368617ca81ad6def5992fb52c029a4a2d8
stephanieboyd <boydstep01@gmail.com>
2020-02-06 09:32:34 +0000

    Fix test to use Andy's new approach

--- ui_automation_tests/fixtures/manage_case.py
+++ ui_automation_tests/fixtures/manage_case.py
@@ -3,15 +3,15 @@ from pytest import fixture
 from shared.tools.utils import get_lite_client
 
 
-@fixture
-def manage_case_status_to_withdrawn(driver, seed_data_config, context):
-    lite_client = get_lite_client(context, seed_data_config)
-    status = lite_client.manage_case_status(context.app_id, "withdrawn")
+@fixture(scope="function")
+def manage_case_status_to_withdrawn(driver, api_client_config, context):
+    lite_client = get_lite_client(context, api_client_config)
+    status = lite_client.cases.manage_case_status(context.app_id, "withdrawn")
     assert status == 200, "Case status not updated to new status"
 
 
-@fixture
-def approve_case(driver, seed_data_config, context):
-    lite_client = get_lite_client(context, seed_data_config)
-    status = lite_client.finalise_case(context.app_id, "approve")
+@fixture(scope="function")
+def approve_case(driver, api_client_config, context):
+    lite_client = get_lite_client(context, api_client_config)
+    status = lite_client.cases.finalise_case(context.app_id, "approve")
     assert status == 200, "Case cannot be finalised"
