cb7a4aeeda82b078b7c1eb55a8ab59fe011947bd
Jan Faracik <j.faracik@kainos.com>
2020-02-12 16:19:45 +0000

    Update forms.py

--- apply_for_a_licence/forms.py
+++ apply_for_a_licence/forms.py
@@ -98,30 +98,33 @@ def export_licence_questions(application_type):
                 if application_type == STANDARD_LICENCE
                 else generic.SAVE_AND_CONTINUE,
             ),
-            goods_categories(),
-            conditional(
+            *conditional(
                 application_type != OPEN_LICENCE,
-                Form(
-                    title=ExportLicenceQuestions.HaveYouBeenInformedQuestion.TITLE,
-                    description=ExportLicenceQuestions.HaveYouBeenInformedQuestion.DESCRIPTION,
-                    questions=[
-                        RadioButtons(
-                            name="have_you_been_informed",
-                            options=[
-                                Option("yes", strings.YES, show_pane="pane_reference_number_on_information_form"),
-                                Option("no", strings.NO),
-                            ],
-                            classes=["govuk-radios--inline"],
-                        ),
-                        TextInput(
-                            title=ExportLicenceQuestions.HaveYouBeenInformedQuestion.WHAT_WAS_THE_REFERENCE_CODE_TITLE,
-                            description=ExportLicenceQuestions.HaveYouBeenInformedQuestion.WHAT_WAS_THE_REFERENCE_CODE_DESCRIPTION,
-                            name="reference_number_on_information_form",
-                            optional=True,
-                        ),
-                    ],
-                    default_button_name=generic.SAVE_AND_CONTINUE,
-                ),
+                [
+                    goods_categories(),
+                    Form(
+                        title=ExportLicenceQuestions.HaveYouBeenInformedQuestion.TITLE,
+                        description=ExportLicenceQuestions.HaveYouBeenInformedQuestion.DESCRIPTION,
+                        questions=[
+                            RadioButtons(
+                                name="have_you_been_informed",
+                                options=[
+                                    Option("yes", strings.YES, show_pane="pane_reference_number_on_information_form"),
+                                    Option("no", strings.NO),
+                                ],
+                                classes=["govuk-radios--inline"],
+                            ),
+                            TextInput(
+                                title=ExportLicenceQuestions.HaveYouBeenInformedQuestion.WHAT_WAS_THE_REFERENCE_CODE_TITLE,
+                                description=ExportLicenceQuestions.HaveYouBeenInformedQuestion.WHAT_WAS_THE_REFERENCE_CODE_DESCRIPTION,
+                                name="reference_number_on_information_form",
+                                optional=True,
+                            ),
+                        ],
+                        default_button_name=generic.SAVE_AND_CONTINUE,
+                    ),
+                ],
+                []
             ),
         ]
     )
