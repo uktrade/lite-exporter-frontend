002ae2267447f43a71d6baa2ec2416557965b3f0
anthony-lord <57621590+anthony-lord@users.noreply.github.com>
2020-02-13 09:52:04 +0000

    Content changes 12 02 20 (#343)
    * Content changes 12 02 20
    
    Lots of small edits, including html files
    
    * Sorted Black issues

--- goods/helpers.py
+++ goods/helpers.py
@@ -10,7 +10,7 @@ def good_summary(good):
     return Summary(
         values={
             "Description": good["description"],
-            "Control list entry": default(good["control_code"], "N/A"),
+            "CLC": default(good["control_code"], "N/A"),
             "Part number": default(good["part_number"], "N/A"),
         },
         classes=["govuk-summary-list--no-border"],
