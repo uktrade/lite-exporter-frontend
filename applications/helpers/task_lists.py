8a582818bf466838cb760751c8ea3415e4676cbd
BenAndrew-Kainos <bena@kainos.com>
2020-02-25 13:53:37 +0000

    Move HMRC into main get_task_list function

--- applications/helpers/task_lists.py
+++ applications/helpers/task_lists.py
@@ -4,7 +4,6 @@ from applications.helpers.task_list_sections import (
     get_reference_number_description,
     get_edit_type,
 )
-from applications.helpers.validate_status import check_all_parties_have_a_document
 from applications.services import (
     get_application_countries,
     get_application_goods_types,
@@ -18,24 +17,11 @@ from conf.constants import (
     EXHIBITION,
     F680,
     GIFTING,
-    NOT_STARTED,
-    DONE,
-    IN_PROGRESS,
     Permissions,
 )
 from core.services import get_sites_on_draft, get_external_locations_on_draft
 from lite_content.lite_exporter_frontend.strings import applications
 from roles.services import get_user_permissions
-
-
-def get_application_task_list(request, application, errors=None):
-    """
-    Returns a correctly formatted task list page for the supplied application
-    """
-    if application["case_type"]["sub_type"]["key"] == HMRC:
-        return _get_hmrc_query_task_list(request, application)
-    else:
-        return _get_task_list(request, application, errors)
 
 
 def _get_strings(application_type):
@@ -51,11 +37,13 @@ def _get_strings(application_type):
         return applications.F680ClearanceTaskList
     elif application_type == GIFTING:
         return applications.GiftingClearanceTaskList
+    elif application_type == HMRC:
+        return applications.HMRCApplicationTaskList
     else:
         raise NotImplementedError(f"No string class for given for {application_type}")
 
 
-def _get_task_list(request, application, errors=None):
+def get_application_task_list(request, application, errors=None):
     user_permissions = get_user_permissions(request)
     additional_documents, _ = get_additional_documents(request, application["id"])
     sites, _ = get_sites_on_draft(request, application["id"])
@@ -70,31 +58,25 @@ def _get_task_list(request, application, errors=None):
         "is_editing": edit[0],
         "edit_type": edit[1],
         "errors": errors,
-        "can_submit": Permissions.SUBMIT_LICENCE_APPLICATION in user_permissions,
-        "supporting_documents": additional_documents["documents"],
-        "locations": sites["sites"] or external_locations["external_locations"],
     }
 
-    if application_type == STANDARD:
-        context["reference_number_description"] = get_reference_number_description(application)
+    if application_type != HMRC:
+        context["can_submit"] = Permissions.SUBMIT_LICENCE_APPLICATION in user_permissions
+        context["supporting_documents"] = additional_documents["documents"]
+        context["locations"] = sites["sites"] or external_locations["external_locations"]
 
-    if application_type == OPEN:
-        context["countries"] = get_application_countries(request, application["id"])
-        context["goodstypes"] = get_application_goods_types(request, application["id"])
+        if application_type == STANDARD:
+            context["reference_number_description"] = get_reference_number_description(application)
+
+        if application_type == OPEN:
+            context["countries"] = get_application_countries(request, application["id"])
+            context["goodstypes"] = get_application_goods_types(request, application["id"])
+        else:
+            context["goods"] = get_application_goods(request, application["id"])
+            context["ultimate_end_users_required"] = True in [good["is_good_incorporated"] for good in context["goods"]]
+
+        return render(request, "applications/task-list.html", context)
+
     else:
-        context["goods"] = get_application_goods(request, application["id"])
-        context["ultimate_end_users_required"] = True in [good["is_good_incorporated"] for good in context["goods"]]
-
-    return render(request, "applications/task-list.html", context)
-
-
-def _get_hmrc_query_task_list(request, application):
-    context = {
-        "application": application,
-        "goods_locations_status": DONE
-        if application["goods_locations"] or application["have_goods_departed"]
-        else NOT_STARTED,
-        "strings": applications.HMRCApplicationTaskList,
-    }
-
-    return render(request, "applications/hmrc-application.html", context)
+        context["locations"] = application["goods_locations"] or application["have_goods_departed"]
+        return render(request, "applications/hmrc-application.html", context)
