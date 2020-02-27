f5304d088836478e48f438585b2e185663757620
Piotr Konopacki <p.konopacki@kainos.com>
2020-02-27 14:08:39 +0000

    Form update

--- applications/forms/f680_details.py
+++ applications/forms/f680_details.py
@@ -2,7 +2,7 @@ from applications.components import back_to_task_list
 from core.services import get_f680_clearance_types
 from lite_content.lite_exporter_frontend.goods import F680Details
 
-from lite_forms.components import Form, Filter, Checkboxes
+from lite_forms.components import Form, Filter, Checkboxes, Option
 
 
 def f680_details_form(application_id):
@@ -10,15 +10,11 @@ def f680_details_form(application_id):
         title=F680Details.TITLE,
         description=F680Details.DESCRIPTION,
         questions=[
-            Filter(),
             Checkboxes(
-                name="f680_clearance_types",
-                options=get_f680_clearance_types(application_id),
-                classes=["govuk-checkboxes--small"],
-                show_select_links=True,
+                name="f680_clearance_types[]",
+                options=[Option(key, value) for key, value in get_f680_clearance_types(application_id).items()]
             ),
         ],
-        javascript_imports=["/assets/javascripts/filter-checkbox-list.js"],
         default_button_name=F680Details.BUTTON,
         back_link=back_to_task_list(application_id),
     )
--- applications/views/f680_details.py
+++ applications/views/f680_details.py
@@ -1,30 +1,13 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
 
 from applications.forms.f680_details import f680_details_form
-from applications.services import get_application, put_application
+from applications.services import put_application
 from lite_forms.views import SingleFormView
 
 
 class F680Details(SingleFormView):
-    # def get(self, request, **kwargs):
-    #     application_id = str(kwargs["pk"])
-    #     application = get_application(request, application_id)
-    #
-    #     return render(request, "applications/f680-details.html", {"application": application})
     def init(self, request, **kwargs):
-        application_id = str(kwargs["pk"])
-        application = get_application(request, application_id)
-
-        print("\n")
-
-        print("application", application)
-
-        print("\n")
-
-        self.data = {}
         self.form = f680_details_form(self.object_pk)
         self.action = put_application
-
+        self.object_pk = str(kwargs["pk"])
         self.success_url = reverse_lazy("applications:task_list", kwargs={"pk": self.object_pk})
