1b59938f40e0a0d76e6efc5618a96161931035ec
Andy Taylor <a.taylor@kainos.com>
2020-02-28 15:15:52 +0000

    re-add UUIDS

--- applications/views/f680_details.py
+++ applications/views/f680_details.py
@@ -10,7 +10,7 @@ class F680Details(SingleFormView):
         self.object_pk = str(kwargs["pk"])
         application = get_application(request, self.object_pk)
         application["f680_clearance_types"] = [
-            f680_clearance_type["id"]["key"] for f680_clearance_type in application["f680_clearance_types"]
+            f680_clearance_type["name"]["key"] for f680_clearance_type in application["f680_clearance_types"]
         ]
         self.form = f680_details_form(request, self.object_pk)
         self.action = put_application
