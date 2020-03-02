b781bafef3216017eab8461f92d0be3f1f529d67
Andy Taylor <a.taylor@kainos.com>
2020-03-02 15:05:42 +0000

    review comments

--- applications/views/f680_details.py
+++ applications/views/f680_details.py
@@ -9,10 +9,11 @@ class F680Details(SingleFormView):
     def init(self, request, **kwargs):
         self.object_pk = str(kwargs["pk"])
         application = get_application(request, self.object_pk)
-        application["f680_clearance_types"] = [
-            f680_clearance_type["name"]["key"] for f680_clearance_type in application["f680_clearance_types"]
-        ]
         self.form = f680_details_form(request, self.object_pk)
         self.action = put_application_with_clearance_types
-        self.data = {"f680_clearance_types": application["f680_clearance_types"]}
+        self.data = {
+            "f680_clearance_types": [
+                f680_clearance_type["name"]["key"] for f680_clearance_type in application["f680_clearance_types"]
+            ]
+        }
         self.success_url = reverse_lazy("applications:task_list", kwargs={"pk": self.object_pk})
