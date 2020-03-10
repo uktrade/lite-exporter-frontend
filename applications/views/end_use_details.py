fe739b3c3976b6074ee97e2962f8a27c86b11d31
Andy Taylor <a.taylor@kainos.com>
2020-03-10 09:01:00 +0000

    fix parsing data without key-value pairs

--- applications/views/end_use_details.py
+++ applications/views/end_use_details.py
@@ -7,7 +7,6 @@ from lite_forms.views import SummaryListFormView
 
 
 class EndUseDetails(SummaryListFormView):
-
     def init(self, request, **kwargs):
         self.object_pk = kwargs["pk"]
         application = get_application(request, self.object_pk)
@@ -23,20 +22,24 @@ class EndUseDetails(SummaryListFormView):
         self.summary_list_button = "Save and continue"
 
     def _parse_end_use_details(self, application):
-        end_use_fields = ["is_military_end_use_controls",
-                          "is_informed_wmd",
-                          "is_suspected_wmd",
-                          "is_eu_military",
-                          ]
+        end_use_detail_questions = [
+            "is_military_end_use_controls",
+            "is_informed_wmd",
+            "is_suspected_wmd",
+            "is_eu_military",
+        ]
+        end_use_detail_references = [
+            "military_end_use_controls_ref",
+            "informed_wmd_ref",
+            "suspected_wmd_ref",
+            "eu_military_ref",
+        ]
 
-        ref_end_use_fields = ["military_end_use_controls_ref",
-                              "informed_wmd_ref",
-                              "suspected_wmd_ref",
-                              "eu_military_ref",
-                              ]
         data = {}
-        for end_use_field in end_use_fields:
-            data[end_use_field] = application.get(end_use_field)["key"]
-        for ref_end_use_field in ref_end_use_fields:
+        for end_use_question in end_use_detail_questions:
+            end_use_question_answer = application.get(end_use_question)
+            if end_use_question_answer:
+                data[end_use_question] = end_use_question_answer["key"]
+        for ref_end_use_field in end_use_detail_references:
             data[ref_end_use_field] = application.get(ref_end_use_field)
         return data
