c2a15a22fb32c66c6630053b8beaf83574e34d9c
Chris Iliev <c.iliev@kainos.com>
2020-03-10 14:05:38 +0000

    refactor parse end user details

--- applications/views/end_use_details.py
+++ applications/views/end_use_details.py
@@ -23,25 +23,24 @@ class EndUseDetails(SummaryListFormView):
 
     @staticmethod
     def _parse_end_use_details(application):
-        end_use_detail_questions = [
-            "is_military_end_use_controls",
-            "is_informed_wmd",
-            "is_suspected_wmd",
-            "is_eu_military",
-        ]
-        end_use_detail_references = [
-            "military_end_use_controls_ref",
-            "informed_wmd_ref",
-            "suspected_wmd_ref",
-            "eu_military_ref",
+
+        end_use_details = [
+            "military_end_use_controls",
+            "informed_wmd",
+            "suspected_wmd",
+            "eu_military",
+            "compliant_limitations_eu",
         ]
 
         data = {}
-        for end_use_question in end_use_detail_questions:
+        for end_use_detail in end_use_details:
+            end_use_question = "is_" + end_use_detail
+            end_use_reference = end_use_detail + "_ref"
+
             application_end_use_question = application.get(end_use_question)
             if application_end_use_question:
                 data[end_use_question] = application_end_use_question
-        for end_use_reference in end_use_detail_references:
+
             application_end_use_reference = application.get(end_use_reference)
             if application_end_use_reference:
                 data[end_use_reference] = application_end_use_reference
