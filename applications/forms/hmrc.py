ad3e9ebd336bfcd39693ff368bbc5a760a3a65ee
BenAndrew-Kainos <bena@kainos.com>
2020-03-25 14:12:24 +0000

    Add reference name form

--- applications/forms/hmrc.py
+++ applications/forms/hmrc.py
@@ -1,7 +1,9 @@
+from django.urls import reverse_lazy
+
 from lite_content.lite_exporter_frontend import strings
 from applications.components import back_to_task_list
 from core.helpers import str_date_only
-from lite_forms.components import Form, TextArea, Summary, HiddenField
+from lite_forms.components import Form, TextArea, TextInput, Summary, HiddenField
 
 
 def confirm_organisation_form(organisation):
@@ -29,4 +31,13 @@ def query_explanation_form(application_id):
         questions=[TextArea(name="reasoning", optional=True, extras={"max_length": 1000,})],
         default_button_name=strings.Hmrc.QueryExplanation.BUTTON_TEXT,
         back_link=back_to_task_list(application_id),
+    )
+
+
+def reference_name_form():
+    return Form(
+        title="Enter a reference for this HMRC Query",
+        description="Give the query a reference name so you can refer back to it when needed.",
+        questions=[TextInput(name="name"),],
+        default_button_name="Continue",
     )
--- hmrc/views.py
+++ hmrc/views.py
@@ -1,14 +1,17 @@
+from http import HTTPStatus
+
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
-from applications.forms.hmrc import confirm_organisation_form
+from applications.forms.hmrc import confirm_organisation_form, reference_name_form
 from applications.services import post_applications
 from conf.constants import CaseTypes
 from core.helpers import convert_dict_to_query_params
 from core.permissions import validate_is_in_organisation_type
 from core.services import get_organisations, get_organisation
 from lite_forms.generators import form_page
+from lite_forms.views import SingleFormView
 
 
 class SelectAnOrganisation(TemplateView):
@@ -47,9 +50,17 @@ class SelectAnOrganisation(TemplateView):
                 # Return an error if the user hasn't selected an organisation
                 return self.get(request, show_error=True, *args, **kwargs)
         else:
-            # Create a draft HMRC application
-            data = {"name": "HMRC query", "application_type": CaseTypes.CRE, "organisation": organisation}
+            return redirect(reverse_lazy("hmrc:reference_name", kwargs={"org_id": organisation}))
 
-            response, _ = post_applications(request, data)
 
+class ReferenceName(TemplateView):
+    def get(self, request, org_id):
+        return form_page(request, reference_name_form())
+
+    def post(self, request, org_id):
+        data = {"name": request.POST["name"], "application_type": CaseTypes.CRE, "organisation": str(org_id)}
+        response, status_code = post_applications(request, data)
+        if status_code != HTTPStatus.CREATED:
+            return form_page(request, reference_name_form(), errors=response.get("errors"))
+        else:
             return redirect(reverse_lazy("applications:task_list", kwargs={"pk": response["id"]}))
