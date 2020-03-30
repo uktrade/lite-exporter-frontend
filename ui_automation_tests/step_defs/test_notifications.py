a55e6027491af2dfd27260c11a8b62ed18be68cc
Andy Taylor <a.taylor@kainos.com>
2020-03-30 15:08:11 +0100

    chore: TODO - API Test Client (#396)

--- ui_automation_tests/step_defs/test_notifications.py
+++ ui_automation_tests/step_defs/test_notifications.py
@@ -3,16 +3,14 @@ from pytest_bdd import scenarios, given, when, then
 from ui_automation_tests.pages.hub_page import Hub
 from ui_automation_tests.pages.shared import Shared
 from ui_automation_tests.shared.tools import helpers
-from ui_automation_tests.shared.tools.utils import get_lite_client
 
 scenarios("../features/notifications.feature", strict_gherkin=False)
 
 
 @given("an application exists and a case note has been added via internal gov site")
-def application_exists_case_note_added(apply_for_standard_application, api_client_config, context, driver):
-    lite_client = get_lite_client(context, api_client_config=api_client_config)
-    lite_client.ecju_queries.add_ecju_query(context.case_id)
-    lite_client.cases.add_case_note(context, context.case_id)
+def application_exists_case_note_added(apply_for_standard_application, api_test_client, context, driver):
+    api_test_client.ecju_queries.add_ecju_query(context.case_id)
+    api_test_client.cases.add_case_note(context, context.case_id)
     context.number_of_notifications = Hub(driver).return_number_of_notifications()
 
 
