6a35b44314e750a069f030c97a6b1ffb3228ae39
Andy Taylor <a.taylor@kainos.com>
2020-03-30 13:50:40 +0100

    http library

--- ui_automation_tests/fixtures/manage_case.py
+++ ui_automation_tests/fixtures/manage_case.py
@@ -1,13 +1,15 @@
+from http import HTTPStatus
+
 from pytest import fixture
 
 
 @fixture(scope="function")
 def manage_case_status_to_withdrawn(api_test_client, context):
     status = api_test_client.cases.manage_case_status(context.app_id, "withdrawn")
-    assert status == 200, "Case status not updated to new status"
+    assert status == HTTPStatus.OK, "Case status not updated to new status"
 
 
 @fixture(scope="function")
 def approve_case(api_test_client, context):
     status = api_test_client.cases.finalise_case(context.app_id, "approve")
-    assert status == 200, "Case cannot be finalised"
+    assert status == HTTPStatus.OK, "Case cannot be finalised"
