d1fe9dd7288370ffa9c372dc47ed457f7f23cafa
BenAndrew-Kainos <bena@kainos.com>
2020-04-01 15:58:59 +0100

    clc filter

--- licences/views.py
+++ licences/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from core.helpers import convert_dict_to_query_params
+from core.services import get_control_list_entries
 from licences.services import get_licences
-from lite_forms.components import FiltersBar, TextInput, HiddenField
+from lite_forms.components import FiltersBar, TextInput, HiddenField, Select
 
 
 class ApplicationsList(TemplateView):
@@ -12,11 +13,13 @@ class ApplicationsList(TemplateView):
             "page": int(request.GET.get("page", 1)),
             "type": request.GET.get("type", "licence"),
             "reference": request.GET.get("reference"),
+            "clc": request.GET.get("clc"),
         }
         licences = get_licences(request, convert_dict_to_query_params(params))
 
         filters = FiltersBar([
             TextInput(name="reference", title="Reference",),
+            Select(name="clc", title="CLC", options=get_control_list_entries(request, convert_to_options=True)),
             HiddenField(name="type", value=params["type"]),
             HiddenField(name="page", value=params["page"]),
         ])
