9c52eaba6863e5b322076df96e43ce95b1f811fe
BenAndrew-Kainos <bena@kainos.com>
2020-04-01 16:46:08 +0100

    filter by country

--- licences/views.py
+++ licences/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from core.helpers import convert_dict_to_query_params
-from core.services import get_control_list_entries
+from core.services import get_control_list_entries, get_countries
 from licences.services import get_licences
 from lite_forms.components import FiltersBar, TextInput, HiddenField, Select
 
@@ -14,12 +14,14 @@ class ApplicationsList(TemplateView):
             "type": request.GET.get("type", "licence"),
             "reference": request.GET.get("reference"),
             "clc": request.GET.get("clc"),
+            "country": request.GET.get("country"),
         }
         licences = get_licences(request, convert_dict_to_query_params(params))
 
         filters = FiltersBar([
             TextInput(name="reference", title="Reference",),
             Select(name="clc", title="CLC", options=get_control_list_entries(request, convert_to_options=True)),
+            Select(name="country", title="Country", options=get_countries(request, convert_to_options=True)),
             HiddenField(name="type", value=params["type"]),
             HiddenField(name="page", value=params["page"]),
         ])
