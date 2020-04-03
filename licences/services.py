ff3c0475b9100ee5814f0870c6411128912efa21
BenAndrew-Kainos <bena@kainos.com>
2020-04-03 15:02:21 +0100

    black

--- licences/services.py
+++ licences/services.py
@@ -4,14 +4,16 @@ from core.helpers import convert_dict_to_query_params
 
 
 def get_licences(request, page, type=None, reference=None, clc=None, country=None, end_user=None, active_only=None):
-    params = convert_dict_to_query_params({
-        "page": page,
-        "type": type,
-        "reference": reference,
-        "clc": clc,
-        "country": country,
-        "end_user": end_user,
-        "active_only": active_only,
-    })
+    params = convert_dict_to_query_params(
+        {
+            "page": page,
+            "type": type,
+            "reference": reference,
+            "clc": clc,
+            "country": country,
+            "end_user": end_user,
+            "active_only": active_only,
+        }
+    )
     data = get(request, LICENCES_URL + "?" + params)
     return data.json()
