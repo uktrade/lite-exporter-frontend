58954f76c0d9bc7a7c9ef75bac06babf687c1d0f
BenAndrew-Kainos <bena@kainos.com>
2020-04-03 15:00:55 +0100

    review comments

--- licences/services.py
+++ licences/services.py
@@ -1,7 +1,17 @@
 from conf.client import get
 from conf.constants import LICENCES_URL
+from core.helpers import convert_dict_to_query_params
 
 
-def get_licences(request, params):
+def get_licences(request, page, type=None, reference=None, clc=None, country=None, end_user=None, active_only=None):
+    params = convert_dict_to_query_params({
+        "page": page,
+        "type": type,
+        "reference": reference,
+        "clc": clc,
+        "country": country,
+        "end_user": end_user,
+        "active_only": active_only,
+    })
     data = get(request, LICENCES_URL + "?" + params)
     return data.json()
--- licences/views.py
+++ licences/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from core.helpers import convert_dict_to_query_params
 from core.services import get_control_list_entries, get_countries
 from licences.services import get_licences
 from lite_content.lite_exporter_frontend.licences import LicencesList
@@ -10,36 +9,47 @@ from lite_forms.components import FiltersBar, TextInput, HiddenField, Select, Ch
 
 class ApplicationsList(TemplateView):
     def get(self, request, **kwargs):
-        params = {
-            "page": int(request.GET.get("page", 1)),
-            "type": request.GET.get("type", "licence"),
-            "reference": request.GET.get("reference"),
-            "clc": request.GET.get("clc"),
-            "country": request.GET.get("country"),
-            "end_user": request.GET.get("end_user"),
-            "active_only": request.GET.get("active_only"),
-        }
-        licences = get_licences(request, convert_dict_to_query_params(params))
+        page = int(request.GET.get("page", 1))
+        type = request.GET.get("type", "licence")
+
+        licences = get_licences(
+            request,
+            page,
+            type=type,
+            reference=request.GET.get("reference"),
+            clc=request.GET.get("clc"),
+            country=request.GET.get("country"),
+            end_user=request.GET.get("end_user"),
+            active_only=request.GET.get("active_only"),
+        )
 
         filters = FiltersBar(
             [
                 TextInput(name="reference", title=LicencesList.Filters.REFERENCE,),
-                Select(name="clc", title=LicencesList.Filters.CLC, options=get_control_list_entries(request, convert_to_options=True)),
-                Select(name="country", title=LicencesList.Filters.DESTINATION_COUNTRY, options=get_countries(request, convert_to_options=True)),
+                Select(
+                    name="clc",
+                    title=LicencesList.Filters.CLC,
+                    options=get_control_list_entries(request, convert_to_options=True),
+                ),
+                Select(
+                    name="country",
+                    title=LicencesList.Filters.DESTINATION_COUNTRY,
+                    options=get_countries(request, convert_to_options=True),
+                ),
                 TextInput(name="end_user", title=LicencesList.Filters.DESTINATION_NAME,),
                 Checkboxes(
                     name="active_only",
                     options=[Option(key=True, value=LicencesList.Filters.ACTIVE)],
                     classes=["govuk-checkboxes--small"],
                 ),
-                HiddenField(name="type", value=params["type"]),
-                HiddenField(name="page", value=params["page"]),
+                HiddenField(name="type", value=type),
+                HiddenField(name="page", value=page),
             ]
         )
 
         context = {
             "licences": licences,
-            "page": params.pop("page"),
+            "page": page,
             "filters": filters,
         }
         return render(request, "licences/licences.html", context)
