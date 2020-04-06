8e98cda7c0a3e516d0a7a5d9536eb612f5178eaa
BenAndrew-Kainos <bena@kainos.com>
2020-04-06 12:00:42 +0100

    set page default to 1

--- licences/services.py
+++ licences/services.py
@@ -4,9 +4,7 @@ from core.helpers import convert_parameters_to_query_params
 
 
 def get_licences(
-    request, page=None, licence_type=None, reference=None, clc=None, country=None, end_user=None, active_only=None
+    request, page=1, licence_type=None, reference=None, clc=None, country=None, end_user=None, active_only=None
 ):
-    if not page:
-        page = 1
     data = get(request, LICENCES_URL + convert_parameters_to_query_params(locals()))
     return data.json()
