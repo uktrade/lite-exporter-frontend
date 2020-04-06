98ede772e9b751d996b87e1288aa52b7ef9f4443
BenAndrew-Kainos <bena@kainos.com>
2020-04-06 11:51:59 +0100

    black

--- licences/services.py
+++ licences/services.py
@@ -3,7 +3,9 @@ from conf.constants import LICENCES_URL
 from core.helpers import convert_dict_to_query_params
 
 
-def get_licences(request, page=None, licence_type=None, reference=None, clc=None, country=None, end_user=None, active_only=None):
+def get_licences(
+    request, page=None, licence_type=None, reference=None, clc=None, country=None, end_user=None, active_only=None
+):
     if not page:
         page = 1
     data = get(request, LICENCES_URL + "?" + convert_dict_to_query_params(locals()))
