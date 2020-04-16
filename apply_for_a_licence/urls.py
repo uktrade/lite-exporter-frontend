bba05cff33d389f3ca8661447cc58d8d23efb4b1
Andy Taylor <a.taylor@kainos.com>
2020-04-16 09:25:42 +0100

    change url

--- apply_for_a_licence/urls.py
+++ apply_for_a_licence/urls.py
@@ -6,10 +6,8 @@ app_name = "apply_for_a_licence"
 
 urlpatterns = [
     path("", views.LicenceType.as_view(), name="start"),
-    path("export-licence/", views.ExportLicenceQuestions.as_view(), name="export_licence_questions"),
-    path(
-        "trade-control-licence/", views.TradeControlLicenceQuestions.as_view(), name="trade_control_licence_questions"
-    ),
+    path("export/", views.ExportLicenceQuestions.as_view(), name="export_licence_questions"),
+    path("trade-control/", views.TradeControlLicenceQuestions.as_view(), name="trade_control_licence_questions"),
     path("transhipment/", views.TranshipmentQuestions.as_view(), name="transhipment_questions"),
     path("mod/", views.MODClearanceQuestions.as_view(), name="mod_questions"),
 ]
