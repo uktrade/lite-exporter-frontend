bbf8911ab5424200ca62a19585c1110e5a5cc96b
Jan Faracik <j.faracik@kainos.com>
2020-04-17 14:40:42 +0100

    fix

--- core/builtins/custom_tags.py
+++ core/builtins/custom_tags.py
@@ -275,12 +275,6 @@ def date_display(value):
         "December",
     ]
 
-    return 'TO DO'
-
-    print()
-    print(value)
-    print()
-
     year, month, day = value.split("-")
     month = months[(int(month) - 1)]
 
