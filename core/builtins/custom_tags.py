b6cbe1aacaf29b1c7a56945c07746163db75978a
BenAndrew-Kainos <bena@kainos.com>
2020-04-17 16:32:11 +0100

    round value

--- core/builtins/custom_tags.py
+++ core/builtins/custom_tags.py
@@ -416,4 +416,4 @@ def abbreviate_string(string, length):
 
 @register.filter()
 def get_usage_value(good):
-    return (good["usage"] / good["licenced_quantity"]) * float(good["licenced_value"])
+    return round((good["usage"] / good["licenced_quantity"]) * float(good["licenced_value"]), 2)
