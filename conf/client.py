f0983492bbd4b7b38fc368f862c707681774d5d0
Eugene Fahy <e.fahy@kainos.com>
2020-04-29 17:10:19 +0100

    Started work on including HAWK signing header in requests to LITE API

--- conf/client.py
+++ conf/client.py
@@ -1,70 +1,72 @@
 import requests
+from mohawk import Sender
+from mohawk.base import HawkEmptyValue
 
 from conf.settings import env
 
 
 def get(request, appended_address):
-    if request:
-        return requests.get(
-            env("LITE_API_URL") + appended_address,
-            headers={
-                "EXPORTER-USER-TOKEN": str(request.user.user_token),
-                "X-Correlation-Id": str(request.correlation),
-                "ORGANISATION-ID": str(request.user.organisation),
-            },
-        )
+    url = env("LITE_API_URL") + appended_address
 
-    return requests.get(env("LITE_API_URL") + appended_address)
+    if request:
+        return requests.get(url, headers=_get_headers(request, url, "GET", {}, "text/plain"),)
+
+    return requests.get(url)
 
 
 def post(request, appended_address, json):
-    return requests.post(
-        env("LITE_API_URL") + appended_address,
-        json=json,
-        headers={
-            "EXPORTER-USER-TOKEN": str(request.user.user_token),
-            "X-Correlation-Id": str(request.correlation),
-            "ORGANISATION-ID": str(request.user.organisation),
-        },
-    )
+    url = env("LITE_API_URL") + appended_address
+
+    return requests.post(url, json=json, headers=_get_headers(request, url, "POST", "application/json", json),)
 
 
 def put(request, appended_address: str, json):
     if not appended_address.endswith("/"):
         appended_address = appended_address + "/"
 
-    return requests.put(
-        env("LITE_API_URL") + appended_address,
-        json=json,
-        headers={
-            "EXPORTER-USER-TOKEN": str(request.user.user_token),
-            "X-Correlation-Id": str(request.correlation),
-            "ORGANISATION-ID": str(request.user.organisation),
-        },
-    )
+    url = env("LITE_API_URL") + appended_address
+
+    return requests.put(url, json=json, headers=_get_headers(request, url, "PUT", "application/json", json),)
 
 
 def patch(request, appended_address: str, json):
     if not appended_address.endswith("/"):
         appended_address = appended_address + "/"
 
+    url = env("LITE_API_URL") + appended_address
+
     return requests.patch(
-        env("LITE_API_URL") + appended_address,
+        url=env("LITE_API_URL") + appended_address,
         json=json,
-        headers={
-            "EXPORTER-USER-TOKEN": str(request.user.user_token),
-            "X-Correlation-Id": str(request.correlation),
-            "ORGANISATION-ID": str(request.user.organisation),
-        },
+        headers=_get_headers(request, url, "PATCH", "application/json", json),
     )
 
 
 def delete(request, appended_address):
+    url = env("LITE_API_URL") + appended_address
+
     return requests.delete(
-        env("LITE_API_URL") + appended_address,
-        headers={
-            "EXPORTER-USER-TOKEN": str(request.user.user_token),
-            "X-Correlation-Id": str(request.correlation),
-            "ORGANISATION-ID": str(request.user.organisation),
-        },
+        url=env("LITE_API_URL") + appended_address,
+        headers=_get_headers(request, url, "DELETE", "application/json", ""),
     )
+
+
+def _get_headers(request, url: str, method: str, content: str, content_type: str):
+    return {
+        "EXPORTER-USER-TOKEN": str(request.user.user_token),
+        "X-Correlation-Id": str(request.correlation),
+        "ORGANISATION-ID": str(request.user.organisation),
+        "Authorization": _get_authorisation_header(url, method, content, content_type),
+    }
+
+
+def _get_authorisation_header(url: str, method: str, content: str, content_type: str):
+    sender = Sender(
+        {"id": "exporter-frontend", "key": "a long, complicated secret", "algorithm": "sha256"},
+        url,
+        method,
+        content=content,
+        content_type=content_type,
+    )
+
+    return sender.request_header
