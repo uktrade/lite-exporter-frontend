5f86f9690bb7789838f260baa87fedb6f506c105
asmund <asmund.bekker@gmail.com>
2020-05-06 12:28:19 +0100

    set permanent by default on transhipment

--- ui_automation_tests/step_defs/test_standard_application.py
+++ ui_automation_tests/step_defs/test_standard_application.py
@@ -251,7 +251,6 @@ def create_standard_individual_transhipment_application(driver, context):  # noq
     functions.click_submit(driver)
     enter_type_of_application(driver, "sitl", context)
     enter_application_name(driver, context)
-    enter_permanent_or_temporary(driver, "permanent", context)
     ApplyForALicencePage(driver).select_goods_categories()
     functions.click_submit(driver)
     enter_export_licence(driver, "yes", "123456", context)
