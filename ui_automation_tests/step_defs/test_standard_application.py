1c7e15c232b34c09c9f009fb2949c91fd4b6be68
stephanieboyd <boydstep01@gmail.com>
2020-05-07 14:32:39 +0100

    fix test

--- ui_automation_tests/step_defs/test_standard_application.py
+++ ui_automation_tests/step_defs/test_standard_application.py
@@ -180,8 +180,8 @@ def create_standard_application(driver, export_type, context):  # noqa
     enter_type_of_application(driver, "siel", context)
     enter_permanent_or_temporary(driver, export_type, context)
     enter_application_name(driver, context)
-    answer_firearms_question(driver)
     enter_export_licence(driver, "yes", "123456", context)
+    answer_firearms_question(driver)
 
 
 @then("I see the application overview")  # noqa
