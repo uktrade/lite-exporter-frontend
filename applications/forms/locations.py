81d5abeb56995c8fc89c4ddf424c4f2c5737852a
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-05-11 12:09:17 +0100

    Black

--- applications/forms/locations.py
+++ applications/forms/locations.py
@@ -82,8 +82,7 @@ def new_external_location_form(request, application_type=None, location_type=Non
     return FormGroup(
         forms=[
             conditional(
-                (application_type in [CaseTypes.SICL, CaseTypes.OICL]),
-                location_type_form(request, application_type),
+                (application_type in [CaseTypes.SICL, CaseTypes.OICL]), location_type_form(request, application_type),
             ),
             new_location_form(application_type, location_type),
         ]
