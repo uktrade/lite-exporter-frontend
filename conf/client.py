e8a41b8a0a76b9b0c51b6d9fc01582acd680d178
Andy Taylor <a.taylor@kainos.com>
2020-05-12 15:34:16 +0100

    fix parser

--- conf/client.py
+++ conf/client.py
@@ -8,7 +8,7 @@ from conf.settings import env
 
 
 def get(request, appended_address):
-    url = _build_absolute_uri(appended_address)
+    url = _build_absolute_uri(appended_address.replace(" ", "%20"))
 
     sender = _get_hawk_sender(url, "GET", "application/json", None)
 
@@ -70,7 +70,7 @@ def delete(request, appended_address):
 
 
 def _build_absolute_uri(appended_address):
-    url = env("LITE_API_URL") + appended_address.replace(" ", "%20")
+    url = env("LITE_API_URL") + appended_address
 
     if not url.endswith("/") and "?" not in url:
         url = url + "/"
