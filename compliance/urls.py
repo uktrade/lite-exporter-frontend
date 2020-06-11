11ab8a500b86ed814b931cb58d284b12c304f627
BenAndrew-Kainos <bena@kainos.com>
2020-06-11 09:28:55 +0100

    Add help page to multi form

--- compliance/urls.py
+++ compliance/urls.py
@@ -5,8 +5,7 @@ from compliance import views
 app_name = "compliance"
 
 urlpatterns = [
-    path("annual-returns/", views.AnnualReturns.as_view(), name="annual_returns"),
-    path("annual-returns/view/", views.AnnualReturnsList.as_view(), name="annual_returns_list"),
+    path("annual-returns/", views.AnnualReturnsList.as_view(), name="annual_returns_list"),
     path("annual-returns/<uuid:pk>/download/", views.AnnualReturnsDownload.as_view(), name="annual_returns_download"),
     path("annual-returns/add/", views.AddAnnualReturn.as_view(), name="add_annual_return"),
 ]
