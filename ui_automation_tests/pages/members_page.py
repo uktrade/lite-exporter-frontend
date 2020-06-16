e6a25e879f2d934df3937a1f695baf63d85424f7
Nick Philpott <16895840+phlptt@users.noreply.github.com>
2020-06-16 13:49:02 +0100

    test: Tidy up exporter june

--- ui_automation_tests/pages/members_page.py
+++ ui_automation_tests/pages/members_page.py
@@ -7,8 +7,6 @@ from ui_automation_tests.shared.tools.helpers import find_paginated_item_by_id
 class MembersPage(BasePage):
     LINK_VIEW_ID_PREFIX = "link-view-"
     BUTTON_ADD_A_MEMBER_ID = "button-add-a-member"
-    BUTTON_APPLY_FILTER_ID = "button-apply-filters"
-    FILTERS_LINK_ID = "show-filters-link"
     STATUS_ID = "status"
 
     def click_view_member_link(self, email: str):
@@ -18,12 +16,6 @@ class MembersPage(BasePage):
     def click_add_a_member_button(self):
         self.driver.find_element_by_id(self.BUTTON_ADD_A_MEMBER_ID).click()
 
-    def click_show_filters_link(self):
-        self.driver.find_element_by_id(self.FILTERS_LINK_ID).click()
-
     def select_filter_status_from_dropdown(self, status):
         select = Select(self.driver.find_element_by_id(self.STATUS_ID))
         select.select_by_visible_text(status)
-
-    def click_apply_filters_button(self):
-        self.driver.find_element_by_id(self.BUTTON_APPLY_FILTER_ID).click()
