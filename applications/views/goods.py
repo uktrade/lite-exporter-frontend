629843cf8ca4c1f20a0506da804402f967acf5c9
Chris Iliev <c.iliev@kainos.com>
2020-06-16 10:46:03 +0100

    edit good details on application

--- applications/views/goods.py
+++ applications/views/goods.py
@@ -22,7 +22,9 @@ from goods.forms import (
     document_grading_form,
     attach_documents_form,
     add_good_form_group,
+    edit_good_details_form_group,
 )
+from goods.helpers import COMPONENT_SELECTION_TO_DETAIL_FIELD_MAP
 from goods.services import (
     get_goods,
     get_good,
@@ -30,6 +32,8 @@ from goods.services import (
     post_good_documents,
     post_good_document_sensitivity,
     validate_good,
+    get_good_details,
+    edit_good_details,
 )
 from lite_forms.components import FiltersBar, TextInput
 from lite_forms.generators import error_page, form_page
@@ -201,6 +205,21 @@ class RemovePreexistingGood(TemplateView):
             return error_page(request, "Unexpected error removing product")
 
         return redirect(reverse_lazy("applications:goods", kwargs={"pk": application_id}))
+
+
+# class EditGoodDetails(MultiFormView):
+#     def init(self, request, **kwargs):
+#         self.object_pk = str(kwargs["pk"])
+#         self.forms = edit_good_details_form_group(request)
+#         self.action = edit_good_details
+#         self.data = get_good_details(request, self.object_pk)[0]
+#         self.success_url = reverse_lazy("applications:add_good_summary", kwargs={"pk": application_id, "good_pk": self.object_pk})
+#
+#     def get_data(self):
+#         if self.data.get("is_component") and self.data.get("component_details"):
+#             detail_field = COMPONENT_SELECTION_TO_DETAIL_FIELD_MAP[self.data["is_component"]]
+#             self.data[detail_field] = self.data["component_details"]
+#         return self.data
 
 
 class GoodsDetailSummaryCheckYourAnswers(TemplateView):
