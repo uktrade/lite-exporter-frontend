fa958333217d43e55039ae6d1a8e9fa90e4ab2ce
BenAndrew-Kainos <bena@kainos.com>
2020-07-06 12:00:08 +0100

    Fix exporter licence pages

--- ui_automation_tests/conftest.py
+++ ui_automation_tests/conftest.py
@@ -651,7 +651,7 @@ def create_licence(context, decision, api_test_client):  # noqa
     )
     api_test_client.cases.add_generated_document(context.case_id, document_template["id"], decision)
     context.generated_document = api_test_client.context["generated_document"]
-    api_test_client.cases.finalise_case(context.case_id, decision)
+    api_test_client.cases.finalise_case(context.case_id, "approve")
     if decision != "no_licence_required":
         api_test_client.cases.finalise_licence(context.case_id)
         context.licence = api_test_client.context["licence"]
