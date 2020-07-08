1a3b6df7bbf60d9a363606bcfb0c46ac8eb5056c
Mark-Spiers-kainos <53567485+Mark-Spiers-kainos@users.noreply.github.com>
2020-07-08 12:24:14 +0100

    Feat: LT-865 - exporter view compliance
    Feat: LT-865 - exporter view compliance

--- goods/views.py
+++ goods/views.py
@@ -13,8 +13,6 @@ from applications.services import (
     get_application_ecju_queries,
     get_case_notes,
     post_case_notes,
-    get_ecju_query,
-    put_ecju_query,
     add_document_data,
     download_document_from_s3,
     get_status_properties,
@@ -22,8 +20,6 @@ from applications.services import (
 )
 from goods.forms import (
     attach_documents_form,
-    respond_to_query_form,
-    ecju_query_respond_confirmation_form,
     delete_good_form,
     document_grading_form,
     raise_a_goods_query,
@@ -58,9 +54,9 @@ from goods.services import (
     get_good_details,
     edit_good_firearm_details,
 )
-from lite_content.lite_exporter_frontend import strings, goods
+from lite_content.lite_exporter_frontend import goods
 from lite_content.lite_exporter_frontend.goods import AttachDocumentForm
-from lite_forms.components import HiddenField, BackLink, FiltersBar, TextInput
+from lite_forms.components import BackLink, FiltersBar, TextInput
 from lite_forms.generators import error_page, form_page
 from lite_forms.views import SingleFormView, MultiFormView
 
@@ -653,83 +649,3 @@ class DeleteDocument(TemplateView):
         delete_good_document(request, good_id, file_pk)
 
         return redirect(reverse("goods:good", kwargs={"pk": good_id}))
-
-
-class RespondToQuery(TemplateView):
-    good_id = None
-    ecju_query_id = None
-    ecju_query = None
-    clc_query_case_id = None
-
-    def dispatch(self, request, *args, **kwargs):
-        self.good_id = str(kwargs["pk"])
-        self.ecju_query_id = str(kwargs["query_pk"])
-
-        good, _ = get_good(request, self.good_id, full_detail=True)
-        self.clc_query_case_id = good["case_id"]
-        self.ecju_query = get_ecju_query(request, self.clc_query_case_id, self.ecju_query_id)
-
-        if self.ecju_query["response"]:
-            return redirect(reverse_lazy("goods:good_detail", kwargs={"pk": self.good_id, "type": "ecju-queries"}))
-
-        return super(RespondToQuery, self).dispatch(request, *args, **kwargs)
-
-    def get(self, request, **kwargs):
-        """
-        Will get a text area form for the user to respond to the ecju_query
-        """
-
-        return form_page(request, respond_to_query_form(self.good_id, self.ecju_query))
-
-    def post(self, request, **kwargs):
-        """
-        will determine what form the user is on:
-        if the user is on the input form will then will determine if data is valid, and move user to confirmation form
-        else will allow the user to confirm they wish to respond and post data if accepted.
-        """
-        form_name = request.POST.get("form_name")
-
-        if form_name == "respond_to_query":
-            # Post the form data to API for validation only
-            data = {"response": request.POST.get("response"), "validate_only": True}
-            response, status_code = put_ecju_query(request, self.clc_query_case_id, self.ecju_query_id, data)
-
-            if status_code != 200:
-                errors = response.get("errors")
-                errors = {error: message for error, message in errors.items()}
-                form = respond_to_query_form(self.clc_query_case_id, self.ecju_query)
-                data = {"response": request.POST.get("response")}
-                return form_page(request, form, data=data, errors=errors)
-            else:
-                form = ecju_query_respond_confirmation_form(
-                    reverse_lazy("goods:respond_to_query", kwargs={"pk": self.good_id, "query_pk": self.ecju_query_id})
-                )
-                form.questions.append(HiddenField("response", request.POST.get("response")))
-                return form_page(request, form)
-        elif form_name == "ecju_query_response_confirmation":
-            if request.POST.get("confirm_response") == "yes":
-                data, status_code = put_ecju_query(request, self.clc_query_case_id, self.ecju_query_id, request.POST)
-                if "errors" in data:
-                    return form_page(
-                        request,
-                        respond_to_query_form(self.good_id, self.ecju_query),
-                        data=request.POST,
-                        errors=data["errors"],
-                    )
-
-                return redirect(reverse_lazy("goods:good_detail", kwargs={"pk": self.good_id, "type": "ecju-queries"}))
-
-            elif request.POST.get("confirm_response") == "no":
-                return form_page(
-                    request, respond_to_query_form(self.clc_query_case_id, self.ecju_query), data=request.POST
-                )
-            else:
-                error = {"required": ["Select yes to confirm you want to delete the file"]}
-                form = ecju_query_respond_confirmation_form(
-                    reverse_lazy("goods:respond_to_query", kwargs={"pk": self.good_id, "query_pk": self.ecju_query_id})
-                )
-                form.questions.append(HiddenField("response", request.POST.get("response")))
-                return form_page(request, form, errors=error)
-        else:
-            # Submitted data does not contain an expected form field - return an error
-            return error_page(request, strings.goods.AttachDocumentPage.UPLOAD_GENERIC_ERROR)
