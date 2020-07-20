58969937c31afb8d1b63c3852602ebecb1b702ed
Mark Spiers <marksp@kainos.com>
2020-07-20 11:42:34 +0100

    increase timeout

--- gconfig.py
+++ gconfig.py
@@ -3,4 +3,4 @@ worker_connections = 1000
 # timeout affect document uploads
 # if timeout is set to be too low, documents that would successfully upload will fail.
 # creating a 502 bad gateway error
-timeout = 600
+timeout = 6000
