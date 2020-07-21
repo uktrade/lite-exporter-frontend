6caa9e1f920d2ad8267354fe34a9a809e371c4f7
BenAndrew-Kainos <53567470+BenAndrew-Kainos@users.noreply.github.com>
2020-07-21 14:36:11 +0100

    bug: Document reupload processing (#482)

--- applications/helpers/reverse_documents.py
+++ applications/helpers/reverse_documents.py
@@ -5,6 +5,9 @@ from applications.services import (
     post_additional_document,
     get_additional_document,
     delete_additional_party_document,
+    post_goods_type_document,
+    get_goods_type_document,
+    delete_goods_type_document,
 )
 from lite_content.lite_exporter_frontend import strings
 
@@ -54,6 +57,15 @@ def document_switch(path):
             "delete": delete_additional_party_document,
             "homepage": "applications:additional_documents",
             "strings": strings.AdditionalDocuments.Documents.AttachDocuments,
+        }
+    elif "goods-types" in path:
+        return {
+            "optional": True,
+            "attach": post_goods_type_document,
+            "download": get_goods_type_document,
+            "delete": delete_goods_type_document,
+            "homepage": "applications:goods_types",
+            "strings": strings.Goods.Documents.AttachDocuments,
         }
     else:
         raise NotImplementedError("document_switch doesn't support this document type")
